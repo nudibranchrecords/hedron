e292a2e6c87c236cc348b63530c1dbe2d96d00e8
Alex Kempton <alex@funwithtriangles.net>
2017-04-02 01:02:19 +0100

    Removed console.log

--- src/containers/CurrentSketch/index.js
+++ src/containers/CurrentSketch/index.js
@@ -3,7 +3,6 @@ import Sketch from '../../components/Sketch'
 
 const mapStateToProps = (state, ownProps) => {
   const sketchId = ownProps.match.params.sketchId
-  console.log(ownProps)
   return {
     title: state.sketches.instances[sketchId].title,
     params: state.sketches.instances[sketchId].paramIds,
