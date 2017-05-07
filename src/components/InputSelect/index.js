0fb1dc9e5482b8d24835e9ae74a5de942879d103
Alex Kempton <alex@funwithtriangles.net>
2017-05-07 18:48:53 +0100

    Lint fixes

--- src/components/InputSelect/index.js
+++ src/components/InputSelect/index.js
@@ -1,20 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import styled from 'styled-components'
 import Select from 'react-select'
-
-const Info = styled.div`
-  font-size: 0.7rem;
-  text-transform: uppercase;
-`
-
-const Label = styled.div`
-  padding: 0.25rem;
-  font-size: 0.7rem;
-  text-transform: uppercase;
-  background: #da5782;
-  color: white;
-`
 
 const options = [
   {
