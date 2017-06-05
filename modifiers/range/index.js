7c128522d8e60523a4b278cc9303673c2559a038
Alex Kempton <alex@funwithtriangles.net>
2017-06-05 22:36:07 +0100

    Modifier range goes below 0 and above 1 for better dynamics

--- modifiers/range/index.js
+++ modifiers/range/index.js
@@ -1,6 +1,9 @@
+const r = 2
+
 module.exports = function (control, val) {
-  const lower = control[0]
-  const upper = control[1]
-  const perc = 1 - (1 - upper) - lower
+  const lower = (control[0] * r * 2) - r
+  const upper = (control[1] * r * 2) - r
+  const perc = r - (r - upper) - lower
   return (val * perc) + lower
 }
+
