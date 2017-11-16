ed5ffe570643b1816d95964c60bf3e13db20f4d4
Alex Kempton <alex@funwithtriangles.net>
2017-11-16 12:37:16 +0000

    Cleanup

--- config/webpack.custom.config.js
+++ config/webpack.custom.config.js
@@ -2,8 +2,6 @@ const devConfig = require('./dev.config.js')
 
 let devServerOptions = {}
 
-console.log(devConfig)
-
 if (devConfig && devConfig.sketchFolder) {
   devServerOptions = {
     contentBase: devConfig.sketchFolder.slice(0, -2),
