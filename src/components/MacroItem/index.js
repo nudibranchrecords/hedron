1fac7fe8a4a939725eb2c70965c3ec1921b3dc93
Alex Kempton <alex@funwithtriangles.net>
2017-11-24 10:59:56 +0000

    Added input settings to macros

--- src/components/MacroItem/index.js
+++ src/components/MacroItem/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import Button from '../Button'
 import Param from '../../containers/Param'
 import MacroLink from '../../containers/MacroLink'
+import InputLink from '../../containers/InputLink'
 import Row from '../Row'
 import styled from 'styled-components'
 
@@ -12,19 +13,40 @@ const Links = styled.div`
   margin-bottom: 0.5rem;
 `
 
-const MacroItem = ({ nodeId, onLearningClick, onDeleteClick, items, isLearning, macroId }) => (
+const MacroItem = ({
+  nodeId, onLearningClick, onDeleteClick, paramLinks,
+  inputLinkIds, isLearning, macroId, inputSettingsAreVisible, paramLinksAreVisible
+}) => (
   <div>
     <Param nodeId={nodeId}>
-      <Links>
-        {items.map(item => (
-          <MacroLink
-            macroId={macroId}
-            nodeId={item.nodeId}
-            paramId={item.paramId}
-            key={item.nodeId}
-          />
-        ))}
-      </Links>
+      {
+        inputSettingsAreVisible &&
+        <div>
+          <h5>Input settings</h5>
+          <Links>
+            {inputLinkIds.map(id => (
+              <InputLink id={id} key={id} />
+            ))}
+          </Links>
+        </div>
+      }
+      {
+        paramLinksAreVisible &&
+        <div>
+          <h5>Connected Params</h5>
+          <Links>
+            {paramLinks.map(item => (
+              <MacroLink
+                macroId={macroId}
+                nodeId={item.nodeId}
+                paramId={item.paramId}
+                key={item.nodeId}
+              />
+            ))}
+          </Links>
+        </div>
+      }
+
       <Row justify='space-between'>
         <Button onClick={onLearningClick}>
           {isLearning ? 'Stop Learning' : 'Start Learning'}
@@ -38,11 +60,14 @@ const MacroItem = ({ nodeId, onLearningClick, onDeleteClick, items, isLearning, 
 
 MacroItem.propTypes = {
   isLearning: PropTypes.bool,
-  items: PropTypes.array.isRequired,
+  paramLinks: PropTypes.array.isRequired,
+  inputLinkIds: PropTypes.array.isRequired,
   nodeId: PropTypes.string.isRequired,
   macroId: PropTypes.string.isRequired,
   onLearningClick: PropTypes.func.isRequired,
-  onDeleteClick: PropTypes.func.isRequired
+  onDeleteClick: PropTypes.func.isRequired,
+  inputSettingsAreVisible: PropTypes.bool,
+  paramLinksAreVisible: PropTypes.bool
 }
 
 export default MacroItem
--- src/containers/MacroItem/index.js
+++ src/containers/MacroItem/index.js
@@ -1,15 +1,21 @@
 import { connect } from 'react-redux'
 import MacroItem from '../../components/MacroItem'
 import getMacro from '../../selectors/getMacro'
+import getNodeInputLinkIds from '../../selectors/getNodeInputLinkIds'
 import { rMacroLearningToggle, uMacroDelete } from '../../store/macros/actions'
 import { values } from 'lodash'
 
 const mapStateToProps = (state, ownProps) => {
   const macro = getMacro(state, ownProps.id)
+  const inputLinkIds = getNodeInputLinkIds(state, macro.nodeId)
+  const paramLinks = values(macro.targetParamLinks)
   return {
     nodeId: macro.nodeId,
     macroId: macro.id,
-    items: values(macro.targetParamLinks),
+    paramLinks,
+    inputLinkIds,
+    inputSettingsAreVisible: inputLinkIds.length > 0,
+    paramLinksAreVisible: paramLinks.length > 0,
     isLearning: state.macros.learningId === ownProps.id
   }
 }
