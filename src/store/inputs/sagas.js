8e8d30edfcb45923dd6fc108021f6e3a7bc74658
Alex Kempton <alex@funwithtriangles.net>
2017-11-30 02:19:40 +0000

    🐎🐎🐎 Simple MIDI input debounce

--- src/store/inputs/sagas.js
+++ src/store/inputs/sagas.js
@@ -10,81 +10,95 @@ import getCurrentBankIndex from '../../selectors/getCurrentBankIndex'
 import lfoProcess from '../../utils/lfoProcess'
 import midiValueProcess from '../../utils/midiValueProcess'
 import { work } from '../../externals/modifiers'
+import debounceInput from '../../utils/debounceInput'
+
+// let frameHasPassed = true
+// let messageCount = 1
 
 export function* handleInput (action) {
   const p = action.payload
+  let messageCount = yield call(debounceInput, p)
+  if (messageCount) {
+    try {
+      const links = yield select(getAssignedLinks, p.inputId)
 
-  try {
-    const links = yield select(getAssignedLinks, p.inputId)
+      for (let i = 0; i < links.length; i++) {
+        let value = p.value
+        let modifiers
+        let skip
+        if (p.meta && p.meta.type === 'midi') {
+          const currentDeviceBank = yield select(getCurrentBankIndex, links[i].deviceId)
+          if (currentDeviceBank !== links[i].bankIndex) skip = true
 
-    for (let i = 0; i < links.length; i++) {
-      let value = p.value
-      let modifiers
-      let skip
-      if (p.meta && p.meta.type === 'midi') {
-        const currentDeviceBank = yield select(getCurrentBankIndex, links[i].deviceId)
-        if (currentDeviceBank !== links[i].bankIndex) skip = true
+          if (!skip) {
+            const currNode = yield select(getNode, links[i].nodeId)
+
+            let midiValue = value
+            value = currNode.value
+
+            while (messageCount--) {
+              value = yield call(midiValueProcess, value, midiValue)
+            }
+          }
+        }
 
         if (!skip) {
-          const currNode = yield select(getNode, links[i].nodeId)
-          value = yield call(midiValueProcess, currNode.value, value)
-        }
-      }
+          if (p.inputId === 'lfo') {
+            let o = yield select(getNodesValues, links[i].lfoOptionIds)
+            value = yield call(lfoProcess, value, o.shape, o.rate)
+          }
 
-      if (!skip) {
-        if (p.inputId === 'lfo') {
-          let o = yield select(getNodesValues, links[i].lfoOptionIds)
-          value = yield call(lfoProcess, value, o.shape, o.rate)
-        }
-
-        if (links[i].modifierIds && links[i].modifierIds.length) {
-          modifiers = yield select(getNodes, links[i].modifierIds)
-          let vals = []
-          for (let j = 0; j < modifiers.length; j++) {
-            const m = modifiers[j]
-            if (!m.type || m.type === p.type) {
-              vals.push(m.value)
-              if (!m.passToNext) {
-                value = yield call(work, m.key, vals, value)
-                value = Math.max(0, Math.min(1, value))
-                vals = []
+          if (links[i].modifierIds && links[i].modifierIds.length) {
+            modifiers = yield select(getNodes, links[i].modifierIds)
+            let vals = []
+            for (let j = 0; j < modifiers.length; j++) {
+              const m = modifiers[j]
+              if (!m.type || m.type === p.type) {
+                vals.push(m.value)
+                if (!m.passToNext) {
+                  value = yield call(work, m.key, vals, value)
+                  value = Math.max(0, Math.min(1, value))
+                  vals = []
+                }
               }
             }
           }
-        }
 
-        switch (links[i].nodeType) {
-          case 'select': {
-            const node = yield select(getNode, links[i].nodeId)
-            const options = node.options
-            value = options[Math.floor(options.length * value)].value
-            break
-          }
-          case 'shot': {
-            const node = yield select(getNode, links[i].nodeId)
-            if (p.meta && p.meta.noteOn) {
-              yield put(inputLinkShotFired(node.sketchId, node.method))
-            } else if (value > 0.5 && links[i].armed) {
-              yield put(inputLinkShotFired(node.sketchId, node.method))
-              yield put(inputLinkShotDisarm(links[i].id))
-            } else if (value < 0.5) {
-              yield put(inputLinkShotArm(links[i].id))
+          switch (links[i].nodeType) {
+            case 'select': {
+              const node = yield select(getNode, links[i].nodeId)
+              const options = node.options
+              value = options[Math.floor(options.length * value)].value
+              break
             }
-            break
-          }
-          case 'macro': {
-            if (p.meta && p.meta.noteOn) {
-              value = 1
+            case 'shot': {
+              const node = yield select(getNode, links[i].nodeId)
+              if (p.meta && p.meta.noteOn) {
+                yield put(inputLinkShotFired(node.sketchId, node.method))
+              } else if (value > 0.5 && links[i].armed) {
+                yield put(inputLinkShotFired(node.sketchId, node.method))
+                yield put(inputLinkShotDisarm(links[i].id))
+              } else if (value < 0.5) {
+                yield put(inputLinkShotArm(links[i].id))
+              }
+              break
+            }
+            case 'macro': {
+              if (p.meta && p.meta.noteOn) {
+                value = 1
+              }
             }
           }
-        }
 
-        yield put(nodeValueUpdate(links[i].nodeId, value, p.meta))
+          yield put(nodeValueUpdate(links[i].nodeId, value, p.meta))
+        }
+        // frameHasPassed = false
+        // messageCount = 1
       }
+    } catch (error) {
+      console.error(error)
+      yield put(projectError(error.message))
     }
-  } catch (error) {
-    console.error(error)
-    yield put(projectError(error.message))
   }
 }
 
