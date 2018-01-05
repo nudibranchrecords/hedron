851a75f76b29b07562efa5e9e93e2a9fcbc5fbd6
Alex Kempton <alex@funwithtriangles.net>
2018-01-05 22:14:04 +0000

    Param node style improved

--- src/components/Param/index.js
+++ src/components/Param/index.js
@@ -6,19 +6,21 @@ import Node from '../Node'
 import OpenButton from '../OpenButton'
 import NodeInputInfo from '../../containers/NodeInputInfo'
 import styled from 'styled-components'
+import inputIcon from '../../assets/icons/input.icon.txt'
+import macroIcon from '../../assets/icons/macro.icon.txt'
+import IconComponent from '../Icon'
+import theme from '../../utils/theme'
 
 const Wrapper = styled(Node)`
-  margin: 0 0 0.5rem;
-`
-
-const InputSelectCol = styled.div`
-  flex: 0 0 5rem;
-  padding-right: 1rem;
+  width: 100%;
+  color: ${theme.textColorLight1};
 `
 
 const BarCol = styled.div`
-  flex: 1;
-  padding-right: 1rem;
+  width: 100%;
+  position: relative;
+  overflow: hidden;
+  margin-bottom: 0.25rem;
 `
 
 const Top = styled.div`
@@ -27,7 +29,7 @@ const Top = styled.div`
 
 const Row = styled.div`
   display: flex;
-  align-items: center;
+  flex-direction: column;
 `
 
 const Bottom = styled.div`
@@ -35,28 +37,62 @@ const Bottom = styled.div`
   align-items: center;
   padding: 0.5rem 0.25rem 0.5rem 0.5rem;
 `
+
 const Title = styled.div`
-  flex: 0 0 8rem;
-  text-align: right;
-  padding-right: 1rem;
+  color: ${theme.textColorLight1};
+  text-transform: uppercase;
+  height: 1rem;
+  font-size: 0.6rem;
+  z-index: 1;
+  position: absolute;
+  top: 0.1rem;
+  left: 0.2rem;
+  width: 1000px;
+  pointer-events: none;
 `
 
+const Icon = styled(IconComponent)`
+  fill: ${theme.textColorLight1};
+  width: 0.6rem;
+  height: 0.6rem;
+  margin-right: 0.1rem;
+`
+const Info = styled.div`
+  display: flex;
+  justify-content: space-between;
+  font-size: 0.6rem;
+
+  > span {
+    display: flex;
+  }
+`
+
+const IconInfo = styled.div`
+  display: flex;
+
+  > span {
+    display: flex;
+    margin-left: 0.3rem;
+  }
+`
 const Param = ({
-  title, nodeId, inputLinkIds, infoText, isOpen, onOpenClick, children
+  title, nodeId, inputLinkIds, infoText, isOpen, onOpenClick, children, value
 }) => (
   <Wrapper>
     <Top>
       <Row>
-        <Title>{title}</Title>
         <BarCol>
+          <Title>{title}</Title>
           <ParamBar nodeId={nodeId} />
         </BarCol>
-        <InputSelectCol>
-          <ParamInputSelect nodeId={nodeId} />
-        </InputSelectCol>
-        <OpenButton onClick={onOpenClick} isOpen={isOpen} />
+        <Info>
+          <span><Icon glyph={inputIcon} />Audio Low</span>
+          <IconInfo>
+            <span><Icon glyph={inputIcon} />1</span>
+            <span><Icon glyph={macroIcon} />0</span>
+          </IconInfo>
+        </Info>
       </Row>
-      <NodeInputInfo nodeId={nodeId} />
     </Top>
     {isOpen &&
       <Bottom>
@@ -75,7 +111,8 @@ Param.propTypes = {
   ),
   isOpen: PropTypes.bool,
   onOpenClick: PropTypes.func.isRequired,
-  children: PropTypes.node
+  children: PropTypes.node,
+  value: PropTypes.string
 }
 
 export default Param
---
+++ src/utils/theme/index.js
@@ -0,0 +1,6 @@
+export default {
+  lineColor1: '#212121',
+  textColorLight1: '#aaa',
+  bgColorDark1: '#333',
+  bgColorDark2: '#222'
+}
