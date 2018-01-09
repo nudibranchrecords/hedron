b83e03799a9d55852da1a4497bb98d8a215f1d3d
Alex Kempton <alex@funwithtriangles.net>
2018-01-09 16:00:13 +0000

    Displaying input links tags

--- src/components/InputLinkMidiControl/index.js
+++ src/components/InputLinkMidiControl/index.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import MidiButton from '../../components/MidiButton'
+import InputLinkTags from '../../components/InputLinkTags'
+import Row from '../../components/Row'
+import styled from 'styled-components'
 
-const InputLinkMidiControl = ({ onAssignClick }) => (
-  <div>
-    <MidiButton onClick={onAssignClick} />
-  </div>
+const Col = styled.div`
+  margin-right: 0.25rem;
+`
+const InputLinkMidiControl = ({ onAssignClick, inputLinkIds }) => (
+  <Row align='center'>
+    <Col>
+      <MidiButton onClick={onAssignClick} />
+    </Col>
+    <InputLinkTags ids={inputLinkIds} />
+  </Row>
 )
 
 InputLinkMidiControl.propTypes = {
-  onAssignClick: PropTypes.func.isRequired
+  onAssignClick: PropTypes.func.isRequired,
+  inputLinkIds: PropTypes.array.isRequired
 }
 
 export default InputLinkMidiControl
--- src/containers/InputLinkMidiControl/index.js
+++ src/containers/InputLinkMidiControl/index.js
@@ -1,10 +1,14 @@
 import { connect } from 'react-redux'
 import InputLinkMidiControl from '../../components/InputLinkMidiControl'
 import { uInputLinkCreate } from '../../store/inputLinks/actions'
+import getLinkableAction from '../../selectors/getLinkableAction'
 
-const mapStateToProps = (state, ownProps) => ({
-
-})
+const mapStateToProps = (state, ownProps) => {
+  const linkableAction = getLinkableAction(state, ownProps.linkableActionId)
+  return {
+    inputLinkIds: linkableAction.inputLinkIds
+  }
+}
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
   onAssignClick: () => { dispatch(uInputLinkCreate(ownProps.linkableActionId, 'midi', 'linkableAction')) }
