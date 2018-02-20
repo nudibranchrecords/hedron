c979c834b8e9a721402dfe61629d27e54b29147d
Alex Kempton <alex@funwithtriangles.net>
2018-02-20 14:46:52 +0000

    Fixed missing input link title in Param

--- src/containers/InputLinkUI/index.js
+++ src/containers/InputLinkUI/index.js
@@ -1,17 +1,13 @@
 import { connect } from 'react-redux'
 import InputLinkUI from '../../components/InputLinkUI'
 import getNode from '../../selectors/getNode'
-import getInputLink from '../../selectors/getInputLink'
 import getInputLinkIdsSeperated from '../../selectors/getInputLinkIdsSeperated'
 
 const mapStateToProps = (state, ownProps) => {
   const node = getNode(state, ownProps.nodeId)
-  const activeInputLinkId = node.activeInputLinkId
-  const activeInputLink = activeInputLinkId && getInputLink(state, activeInputLinkId)
   const inputLinkIds = getInputLinkIdsSeperated(state, ownProps.nodeId)
   return {
     inputLinkIds,
-    inputLinkTitle: activeInputLink && activeInputLink.title,
     currentInputLinkId: node.openedLinkId
   }
 }
--- src/containers/Param/index.js
+++ src/containers/Param/index.js
@@ -3,6 +3,7 @@ import Param from '../../components/Param'
 import getNode from '../../selectors/getNode'
 import { sketchNodeOpenedToggle } from '../../store/sketches/actions'
 import getIsSketchNodeOpened from '../../selectors/getIsSketchNodeOpened'
+import getInputLink from '../../selectors/getInputLink'
 import { nodeShotFired } from '../../store/nodes/actions'
 
 const mapStateToProps = (state, ownProps) => {
@@ -11,12 +12,16 @@ const mapStateToProps = (state, ownProps) => {
   const param = state.nodes[ownProps.nodeId]
   const type = ownProps.type || 'param'
 
+  const activeInputLinkId = node.activeInputLinkId
+  const activeInputLink = activeInputLinkId && getInputLink(state, activeInputLinkId)
+
   return {
     type,
     numInputs: inputLinkIds.length,
     numMacros: node.connectedMacroIds.length,
     title: param.title,
-    isOpen: getIsSketchNodeOpened(state, ownProps.sketchId, ownProps.nodeId, type)
+    isOpen: getIsSketchNodeOpened(state, ownProps.sketchId, ownProps.nodeId, type),
+    inputLinkTitle: activeInputLink && activeInputLink.title
   }
 }
 
