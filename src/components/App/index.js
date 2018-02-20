f3f39ebf8eadd5f164773c925aa7e36df1d520f2
Alex Kempton <alex@funwithtriangles.net>
2018-02-20 18:18:55 +0000

    Macro button higher up in nav

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -36,7 +36,6 @@ const Right = styled.div`
 const Bar = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
   flex: 0 0 3rem;
   background: #111;
   height: 100%;
