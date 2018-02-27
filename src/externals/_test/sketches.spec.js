23f5d045df5850e92f42775153c0c34c397c813f
Alex Kempton <alex@funwithtriangles.net>
2018-02-27 14:15:04 +0000

    Errors thrown when sketch folder is bad

--- src/externals/_test/sketches.spec.js
+++ src/externals/_test/sketches.spec.js
@@ -6,7 +6,11 @@ import sinon from 'sinon'
 proxyquire.noCallThru()
 
 const syncStub = sinon.stub()
+
+syncStub.returns([])
 syncStub.withArgs('foo/bar/*').returns(['foo/bar/dog', 'foo/bar/cat', 'foo/bar/frog'])
+syncStub.withArgs('bar/bar/*').returns(['bar/bar/dog', 'bar/bar/cat'])
+syncStub.withArgs('wee/bar/*').returns(['wee/bar/dog'])
 
 const { getSketches } = proxyquire('../sketches', {
   glob: {
@@ -14,16 +18,24 @@ const { getSketches } = proxyquire('../sketches', {
     sync: syncStub
   },
   // Mocked up modules and meta (just returning strings for test)
-  [path.resolve('foo/bar/dog')]: 'dogModule',
-  [path.resolve('foo/bar/cat')]: 'catModule',
-  [path.resolve('foo/bar/frog')]: 'frogModule',
+  [path.resolve('foo/bar/dog/index.js')]: 'dogModule',
+  [path.resolve('foo/bar/cat/index.js')]: 'catModule',
+  [path.resolve('foo/bar/frog/index.js')]: 'frogModule',
   [path.resolve('foo/bar/dog/config.js')]: 'dogMeta',
   [path.resolve('foo/bar/cat/config.js')]: 'catMeta',
-  [path.resolve('foo/bar/frog/config.js')]: 'frogMeta'
+  [path.resolve('foo/bar/frog/config.js')]: 'frogMeta',
+
+  // /bar/bar/cat does not have a config file
+  [path.resolve('bar/bar/dog/index.js')]: 'dogModule',
+  [path.resolve('bar/bar/cat/index.js')]: 'catModule',
+  [path.resolve('bar/bar/dog/config.js')]: 'dogMeta',
+
+  // does not have index file
+  [path.resolve('wee/bar/dog/config.js')]: 'dogMeta'
+
 })
 
 test('(External) sketches - getSketches()', (t) => {
-  t.plan(1)
   const expected = {
     dog: {
       Module: 'dogModule',
@@ -40,4 +52,26 @@ test('(External) sketches - getSketches()', (t) => {
   }
   const actual = getSketches('foo/bar')
   t.deepEqual(actual, expected, 'Returns modules from files')
+  t.end()
+})
+
+test('(External) sketches - getSketches() - one module does not have config', (t) => {
+  t.throws(() => {
+    getSketches('bar/bar')
+  })
+  t.end()
+})
+
+test('(External) sketches - getSketches() - one module does not have index', (t) => {
+  t.throws(() => {
+    getSketches('wee/bar')
+  })
+  t.end()
+})
+
+test('(External) sketches - getSketches() - folder doesnt exist', (t) => {
+  t.throws(() => {
+    getSketches('foo/@@@@')
+  })
+  t.end()
 })
