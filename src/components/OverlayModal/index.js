efc0ece986a7af2a6bb11faf052b3e6512d5d945
Alex Kempton <alex@funwithtriangles.net>
2018-02-28 23:05:32 +0000

    Lint fix

--- src/components/OverlayModal/index.js
+++ src/components/OverlayModal/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import Button from '../Button'
 
 const Wrapper = styled.div`
   position: fixed;
