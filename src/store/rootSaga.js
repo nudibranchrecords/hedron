c1a66bb935650be71fb90321aa5248aa367f4f3a
Alex Kempton <alex@funwithtriangles.net>
2018-02-28 19:10:11 +0000

    Uncommented code that shouldnt be commented

--- src/store/rootSaga.js
+++ src/store/rootSaga.js
@@ -6,19 +6,19 @@ import { watchInputLinks } from './inputLinks/sagas'
 import { watchScene } from './scene/sagas'
 import { watchInputs } from './inputs/sagas'
 import { watchClock } from './clock/sagas'
-// import { watchWindows } from './windows/sagas'
+import { watchWindows } from './windows/sagas'
 import { watchMacros } from './macros/sagas'
 
 export default function* rootSaga (dispatch) {
   yield [
     fork(watchProject, dispatch),
     fork(watchScene),
-  // fork(watchSketches),
+    fork(watchSketches),
     fork(watchInputs),
     fork(watchInputLinks),
     fork(watchNodes),
     fork(watchClock),
-  //  fork(watchWindows),
+    fork(watchWindows),
     fork(watchMacros)
   ]
 }
