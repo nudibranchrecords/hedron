3f3dd944886d3ba0b47ece7dfb9134418f17346a
jlchapa <luis.chapa01@gmail.com>
2018-03-08 00:24:49 -0800

    Add a double backslash for windows paths to load sketches

--- src/externals/sketches.js
+++ src/externals/sketches.js
@@ -8,10 +8,19 @@ const getSketches = globUrl => {
     glob.sync(globUrl + '/*').forEach(function (file) {
       const name = path.parse(file).name
       const url = path.resolve(file)
+      let indexUrl = path.format({ dir: url, base: 'index.js' })
+      let configUrl = path.format({ dir: url, base: 'config.js' })
+
+      if (indexUrl.indexOf('\\')) {
+        // For paths in Windows the next require function requires the path to have \\ as a separator instead of only \
+        indexUrl = indexUrl.replace(/\\/g, '\\\\')
+        configUrl = configUrl.replace(/\\/g, '\\\\')
+      }
+
       all[name] = {
         /*eslint-disable */
-        Module: eval('require("' + url + '/index.js")'),
-        config: eval('require("' + url + '/config.js")')
+        Module: eval('require("' + indexUrl + '")'),
+        config: eval('require("' + configUrl + '")')
         /*eslint-enable */
       }
     })
