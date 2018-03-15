d4fb611cccd96e0a055b7882f38dd49a906a1de1
Alex Kempton <alex@funwithtriangles.net>
2018-03-15 11:38:29 +0000

    Antialiasing in settings

--- src/containers/Settings/index.js
+++ src/containers/Settings/index.js
@@ -12,7 +12,9 @@ const mapStateToProps = (state, ownProps) => ({
 const mapDispatchToProps = (dispatch, ownProps) => ({
   onChange: (values) => {
     dispatch(settingsUpdate(values))
+    uiEventEmitter.emit('reset-renderer')
     uiEventEmitter.emit('repaint')
+
   }
 })
 
