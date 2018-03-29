acfcc4ba10d46bf0ae3f650d748ad8c9b3fdec4e
Alex Kempton <alex@funwithtriangles.net>
2018-03-29 16:30:06 +0100

    Scene class created

---
+++ src/engine/Scene.js
@@ -0,0 +1,11 @@
+import * as THREE from 'three'
+
+class Scene {
+  constructor () {
+    this.scene = new THREE.Scene()
+    this.camera = new THREE.PerspectiveCamera(75, null, 1, 1000000)
+    this.camera.position.z = 1000
+  }
+}
+
+export default Scene
