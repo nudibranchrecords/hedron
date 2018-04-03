c1f5d14bae0c57533df8d2141fd0c97a27fbb5d7
Alex Kempton <alex@funwithtriangles.net>
2018-04-03 11:22:34 +0100

    Extra styles for SettingsBox

--- src/components/AudioAnalyzer/index.js
+++ src/components/AudioAnalyzer/index.js
@@ -2,14 +2,56 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import uiEventEmitter from '../../utils/uiEventEmitter'
 import Control from '../../containers/Control'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import theme from '../../utils/theme'
 
 let height, val, offset, hue, i, inputs, bands
 
+const triH = 8
+
+const Wrapper = styled.div`
+  position: relative;
+`
+
+const Container = styled.div`
+  cursor: pointer;
+`
+
+const triangle = css`
+  position: absolute;
+  left: 50%;
+  top: -${triH}px;
+  margin-left: -${triH}px;
+  content: "";
+  display: block;
+  width: 0;
+  height: 0;
+  border-bottom: ${triH}px solid ${theme.bgColorDark1};;
+  border-left: ${triH}px solid transparent;
+  border-right: ${triH}px solid transparent;
+`
+
 const SettingsBox = styled.div`
-   background: ${theme.bgColorDark2};
-   position: absolute;
+  left: 50%;
+  width: 9rem;
+  margin-left: -4.5rem;
+  margin-top: 0.25rem;
+  border: 1px solid white;
+  background: ${theme.bgColorDark1};
+  position: absolute;
+  padding: 0.5rem 0.25rem 0.25rem 0.5rem;
+
+  &:after {
+    ${triangle}
+  }
+
+  &:before {
+    ${triangle}
+    border-bottom-color: white;
+    border-width: ${triH + 2}px;
+    margin-left: -${triH + 2}px;
+    margin-top: -2px;
+  }
 `
 
 class AudioAnalyzer extends React.Component {
@@ -68,17 +110,18 @@ class AudioAnalyzer extends React.Component {
 
   render () {
     return (
-      <div>
-        <canvas ref={node => { this.canvas = node }}
-          onMouseDown={this.props.onMouseDown || this.handleMouseDown}
-      />
+      <Wrapper>
+        <Container>
+          <canvas ref={node => { this.canvas = node }}
+            onMouseDown={this.props.onMouseDown || this.handleMouseDown} />
+        </Container>
         {this.isOpen &&
-        <SettingsBox>
-          <Control nodeId='audioNormalizeLevels' />
-          <Control nodeId='audioLevelsFalloff' />
-        </SettingsBox>
-      }
-      </div>
+          <SettingsBox>
+            <Control nodeId='audioNormalizeLevels' />
+            <Control nodeId='audioLevelsFalloff' />
+          </SettingsBox>
+        }
+      </Wrapper>
     )
   }
 }
