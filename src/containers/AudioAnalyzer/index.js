92a93da3c7775e8cd9a9b9b6610d670739123a5d
Alex Kempton <alex@funwithtriangles.net>
2018-04-03 11:46:18 +0100

    AudioAnalyzer connected using container

---
+++ src/containers/AudioAnalyzer/index.js
@@ -0,0 +1,27 @@
+import { connect } from 'react-redux'
+import AudioAnalyzer from '../../components/AudioAnalyzer'
+import getUiIsEditingType from '../../selectors/getUiIsEditingType'
+import { uiEditingToggle } from '../../store/ui/actions'
+
+const mapStateToProps = (state, ownProps) => (
+  {
+    isOpen: getUiIsEditingType(state) === 'audioAnalyzerPanel'
+  }
+)
+
+const mapDispatchToProps = (dispatch, ownProps) => (
+  {
+    onAnalyzerClick: () => { dispatch(uiEditingToggle('audioAnalyzerPanel')) }
+  }
+)
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+  null,
+  {
+    // Audio analyzer gets state for bars via context
+    // so we only need to check UI to see if panel is open
+    areStatesEqual: (next, prev) => next.ui === prev.ui
+  }
+)(AudioAnalyzer)
