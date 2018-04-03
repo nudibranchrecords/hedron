6328806cdff5403e0ea95a64c820473d71c2664d
Alex Kempton <alex@funwithtriangles.net>
2018-04-03 17:17:19 +0100

    Example projects updated to work with scenes

--- example-projects/trippy/sketches/blobs/index.js
+++ example-projects/trippy/sketches/blobs/index.js
@@ -3,15 +3,14 @@ const { MarchingCubes } = require('three-addons')
 const TWEEN = require('@tweenjs/tween.js')
 
 class Blobs {
-  constructor (world) {
-    const { renderer, scene } = world
+  constructor (injectedScene) {
+    const { scene, renderer } = injectedScene
 
     this.root = new Object3D()
     const scale = 1000
-    this.renderer = renderer
     this.scene = scene
-
-    this.cubeCamera = new CubeCamera(1, 5000, 64)
+    this.renderer = renderer
+    this.cubeCamera = new CubeCamera(1, 3000, 32)
 
     setTimeout(() => {
       this.cubeCamera.update(this.renderer, this.scene)
