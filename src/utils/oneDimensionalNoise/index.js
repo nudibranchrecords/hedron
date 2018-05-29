ae00c99968a04698f3f4898c682456674c295f4f
pcASUS_grey\esnho <esnhoesnho@gmail.com>
2018-05-29 21:43:43 +0200

    fixed lint issues

--- src/utils/oneDimensionalNoise/index.js
+++ src/utils/oneDimensionalNoise/index.js
@@ -2,31 +2,32 @@
 
 // Must be power of 2 to work with bitwise modulo
 const MAX_VERTICES = 512
-const MAX_VERTICES_MASK = MAX_VERTICES -1
+const MAX_VERTICES_MASK = MAX_VERTICES - 1
 
 function lerp (a, b, t) {
-    return a * ( 1 - t ) + b * t
+  return a * (1 - t) + b * t
 }
 
 class OneDimensionalNoise {
-  constructor(scale) {
+  constructor (scale) {
     this.buffer = []
+    this.call = 0
 
-    for ( var i = 0; i < MAX_VERTICES; ++i ) {
-        this.buffer.push(Math.random())
+    for (var i = 0; i < MAX_VERTICES; ++i) {
+      this.buffer.push(Math.random())
     }
   }
 
-  getValue(x) {
+  getValue (x) {
     const xFloor = Math.floor(x)
     const t = x - xFloor
-    const tRemapSmoothstep = t * t * ( 3 - 2 * t )
+    const tRemapSmoothstep = t * t * (3 - 2 * t)
 
-    /// Modulo using &
+    // Modulo using &
     const xMin = xFloor & MAX_VERTICES_MASK
-    const xMax = ( xMin + 1 ) & MAX_VERTICES_MASK
+    const xMax = (xMin + 1) & MAX_VERTICES_MASK
 
-    const y = lerp( this.buffer[ xMin ], this.buffer[ xMax ], tRemapSmoothstep )
+    const y = lerp(this.buffer[xMin], this.buffer[xMax], tRemapSmoothstep)
 
     return y
   }
