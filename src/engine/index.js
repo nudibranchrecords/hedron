be40b3762ab9a35058c294b5be545753a27b6918
cale <cale.bradbury@twistedoakstudios.com>
2018-07-09 13:14:54 -0300

    formatting fix

--- src/engine/index.js
+++ src/engine/index.js
@@ -67,8 +67,9 @@ export const addSketchToScene = (sceneId, sketchId, moduleId) => {
 export const removeSketchFromScene = (sceneId, sketchId) => {
   const sketch = sketches[sketchId]
   scenes[sceneId].scene.remove(sketch.root)
-  if(sketch.destructor)
-    sketch.destructor(scenes[sceneId]);
+  if (sketch.destructor){
+    sketch.destructor(scenes[sceneId])
+  }
   delete sketches[sketchId]
 }
 
