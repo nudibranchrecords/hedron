c86895fb52485a6a2a8515ac55cf37bc32c374b6
cale <cale.bradbury@twistedoakstudios.com>
2018-07-09 16:33:47 -0300

    lint fix

--- src/selectors/getSketchParamId.js
+++ src/selectors/getSketchParamId.js
@@ -1,12 +1,12 @@
 // Gets newest sketch param id by key
 export default (state, sketchId, paramKey) => {
   const sketchParams = state.sketches[sketchId].paramIds
-  for(var i = 0; i<sketchParams.length; i++){
-    var id = sketchParams[i];
+  for (var i = 0; i < sketchParams.length; i++) {
+    var id = sketchParams[i]
     const param = state.nodes[id]
-    if(param.key == paramKey){
-      return id;
+    if (param.key == paramKey) {
+      return id
     }
   }
-  return null;
+  return null
 }
