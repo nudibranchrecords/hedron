e574301d30bdf737ed5b14a09f18cfdd8ac20e00
cale <cale.bradbury@gmail.com>
2018-07-17 09:45:58 -0400

    changed comment to be more acurate and ran a lint fix

--- src/selectors/getSketchParamId.js
+++ src/selectors/getSketchParamId.js
@@ -1,4 +1,4 @@
-// Gets newest sketch param id by key
+// Gets sketch param id by key and sketh id
 export default (state, sketchId, paramKey) => {
   const sketchParams = state.sketches[sketchId].paramIds
   for (var i = 0; i < sketchParams.length; i++) {
