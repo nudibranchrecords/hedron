945b67ab3de7b30b8daace3baf16b1f27831aa6a
Alex Kempton <alex@funwithtriangles.net>
2018-09-07 15:53:28 +0100

    Added trig example project

---
+++ example-projects/trig/sketches/point/index.js
@@ -0,0 +1,24 @@
+const THREE = require('three')
+
+class Point {
+
+  constructor (scene, meta, params) {
+    this.root = new THREE.Group()
+    const geom = new THREE.IcosahedronGeometry(100)
+    const mat = new THREE.MeshNormalMaterial()
+    this.mesh = new THREE.Mesh(geom, mat)
+
+    this.root.add(this.mesh)
+  }
+
+  update (params, time, frameDiff, allParams) {
+    const rad = 1500
+
+    this.mesh.position.x = (rad * params.posX) - rad / 2
+    this.mesh.position.y = (rad * params.posY) - rad / 2
+    this.mesh.position.z = (rad * params.posZ) - rad / 2
+  }
+
+}
+
+module.exports = Point
