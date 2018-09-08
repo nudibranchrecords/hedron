dc3af4eda9a913bfe016ba38c83fdcd42b5a2722
Alex Kempton <alex@funwithtriangles.net>
2018-09-08 16:10:37 +0100

    Form option now for any value bar

--- src/containers/ParamBar/index.js
+++ src/containers/ParamBar/index.js
@@ -3,6 +3,8 @@ import ValueBar from '../../components/ValueBar'
 import { nodeValueUpdate } from '../../store/nodes/actions'
 import getNode from '../../selectors/getNode'
 import getInputLink from '../../selectors/getInputLink'
+import getUiIsEditingNode from '../../selectors/getUiIsEditingNode'
+import { uiEditingOpen } from '../../store/ui/actions'
 
 const mapStateToProps = (state, ownProps) => {
   const node = getNode(state, ownProps.nodeId)
@@ -10,18 +12,26 @@ const mapStateToProps = (state, ownProps) => {
   const linkId = node.activeInputLinkId
   const inputLink = getInputLink(state, linkId)
   const hideBar = type === 'shot' && (!inputLink || inputLink && inputLink.input.type !== 'audio')
+  const editingNode = getUiIsEditingNode(state)
 
   return {
     type,
     hideBar,
-    markerIsVisible: type === 'shot' && !hideBar && inputLink.armed
+    markerIsVisible: type === 'shot' && !hideBar && inputLink.armed,
+    formIsVisible: editingNode && editingNode.id === ownProps.nodeId
   }
 }
 
 const mapDispatchToProps = (dispatch, ownProps) => {
+  const type = ownProps.type || 'param'
+
   return {
     onChange: (value) => {
       dispatch(nodeValueUpdate(ownProps.nodeId, value))
+    },
+    onDoubleClick: () => {
+      type === 'param' &&
+      dispatch(uiEditingOpen('paramValue', ownProps.nodeId))
     }
   }
 }
