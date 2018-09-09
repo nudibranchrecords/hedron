c41b22caa06c394458e75e0fe655dc8c51424714
Alex Kempton <alex@funwithtriangles.net>
2018-09-10 00:26:06 +0100

    Advanced open/close using ui state

--- src/containers/SketchParam/index.js
+++ src/containers/SketchParam/index.js
@@ -1,6 +1,20 @@
+import { connect } from 'react-redux'
 import SketchParam from '../../components/SketchParam'
 import withDeferRender from '../../utils/withDeferRender'
+import { uiAuxToggleOpen } from '../../store/ui/actions'
+import getIsAuxOpen from '../../selectors/getIsAuxOpen'
 
-const ParamContainer = withDeferRender(SketchParam)
+const mapStateToProps = (state, ownProps) => ({
+  advancedIsOpen: getIsAuxOpen(state, ownProps.nodeId)
+})
 
-export default ParamContainer
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  onAdvancedClick: () => {
+    dispatch(uiAuxToggleOpen(ownProps.nodeId))
+  }
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withDeferRender(SketchParam))
