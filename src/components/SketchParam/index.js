b5484ea37838a89d3e24a6e03a72b0467f101786
Alex Kempton <alex@funwithtriangles.net>
2018-09-09 23:43:32 +0100

    Revealer component created

--- src/components/SketchParam/index.js
+++ src/components/SketchParam/index.js
@@ -1,16 +1,26 @@
 import React from 'react'
+import styled from 'styled-components'
 import PropTypes from 'prop-types'
-import Collapsible from '../../containers/Collapsible'
+import Revealer from '../../components/Revealer'
 import Param from '../../containers/Param'
 import Range from '../../containers/Range'
 import InputLinkUI from '../../containers/InputLinkUI'
+import theme from '../../utils/theme'
+
+const Top = styled.div`
+  margin-bottom: 0.5rem;
+  padding-bottom: 0.5rem;
+  border-bottom: ${theme.lineColor1} 1px dashed;
+`
 
 const SketchParam = ({ nodeId, sketchId }) => (
   <Param nodeId={nodeId} sketchId={sketchId}>
-    <Collapsible {... { title:'Advanced' }}>
+    <Top>
+      <InputLinkUI nodeId={nodeId} />
+    </Top>
+    <Revealer title='Advanced' isOpen>
       <Range nodeId={nodeId} />
-    </Collapsible>
-    <InputLinkUI nodeId={nodeId} />
+    </Revealer>
   </Param>
 )
 
--- src/containers/Collapsible/index.js
+++
@@ -1,4 +0,0 @@
-import { connect } from 'react-redux'
-import CollapsibleComponent from '../../components/Collapsible'
-
-export default connect()(CollapsibleComponent)
