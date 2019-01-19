cb34a9ce9d06430b4bc0f0df817acd59562215de
pcASUS_grey\esnho <esnhoesnho@gmail.com>
2019-01-19 17:27:30 +0100

    Updated noise function, added more buffers in order to differentiate each call

--- src/utils/oneDimensionalNoise/index.js
+++ src/utils/oneDimensionalNoise/index.js
@@ -3,31 +3,40 @@
 // Must be power of 2 to work with bitwise modulo
 const MAX_VERTICES = 512
 const MAX_VERTICES_MASK = MAX_VERTICES - 1
+const BUFFERS_COUNT = 'abcdefghijklmnopqrstuvwyxz'.length
 
 function lerp (a, b, t) {
   return a * (1 - t) + b * t
 }
 
 class OneDimensionalNoise {
-  constructor (scale) {
-    this.buffer = []
-    this.call = 0
+  constructor () {
+    this.buffers = []
 
-    for (let i = 0; i < MAX_VERTICES; ++i) {
-      this.buffer.push(Math.random())
+    for (let i = 0; i < BUFFERS_COUNT; ++i) {
+      this.buffers[i] = []
+      for (let j = 0; j < MAX_VERTICES; j++) {
+        this.buffers[i].push(Math.random())
+      }
     }
   }
 
-  getValue (x) {
+  getValue (x, id) {
     const xFloor = Math.floor(x)
     const t = x - xFloor
     const tRemapSmoothstep = t * t * (3 - 2 * t)
+    const bufferId = id.charCodeAt(0) & BUFFERS_COUNT
+
+    let offset = 0;
+    for (let i = 1; i < id.length; i++) {
+      offset += id.charCodeAt(i)
+    }
 
     // Modulo using &
-    const xMin = xFloor & MAX_VERTICES_MASK
+    const xMin = (xFloor + offset) & MAX_VERTICES_MASK
     const xMax = (xMin + 1) & MAX_VERTICES_MASK
 
-    const y = lerp(this.buffer[xMin], this.buffer[xMax], tRemapSmoothstep)
+    const y = lerp(this.buffers[bufferId][xMin], this.buffers[bufferId][xMax], tRemapSmoothstep)
 
     return y
   }
