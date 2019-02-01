4cb23193829da33ec8f3a27e8077a7fc2527d223
Alex Kempton <alex@funwithtriangles.net>
2019-02-01 13:34:48 +0000

    Fixed casing issue

--- src/containers/MacroPropertiesPanel/index.js
+++ src/containers/MacroPropertiesPanel/index.js
@@ -2,7 +2,7 @@ import { connect } from 'react-redux'
 import PropertiesPanel from '../../containers/PropertiesPanel'
 import MacroProperties from '../MacroProperties'
 import getOpenedMacroNode from '../../selectors/getOpenedMacroNode'
-import { rMacroClose } from '../../store/Macros/actions'
+import { rMacroClose } from '../../store/macros/actions'
 
 const mapStateToProps = (state) => {
   const node = getOpenedMacroNode(state)
