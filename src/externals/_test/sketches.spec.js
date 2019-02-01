230eb76139e67aaf54e6a96baaf57a57467fa516
cale <cale.bradbury@gmail.com>
2019-01-31 19:09:05 -0500

    updated tests to support new fs functions and filePathArray that is added to the config

--- src/externals/_test/sketches.spec.js
+++ src/externals/_test/sketches.spec.js
@@ -11,27 +11,31 @@ syncStub.returns([])
 syncStub.withArgs('foo/bar/*').returns(['foo/bar/dog', 'foo/bar/cat', 'foo/bar/frog'])
 syncStub.withArgs('bar/bar/*').returns(['bar/bar/dog', 'bar/bar/cat'])
 syncStub.withArgs('wee/bar/*').returns(['wee/bar/dog'])
+const fsStub = {}
+fsStub.existsSync = (path) => (true)
+fsStub.statSync = (path) => ({ isDirectory:true })
 
 const { loadSketches } = proxyquire('../sketches', {
   glob: {
     // Mocked up sketch files
     sync: syncStub,
   },
+  fs: fsStub,
   // Mocked up modules and meta (just returning strings for test)
   [path.resolve('foo/bar/dog/index.js')]: 'dogModule',
   [path.resolve('foo/bar/cat/index.js')]: 'catModule',
   [path.resolve('foo/bar/frog/index.js')]: 'frogModule',
-  [path.resolve('foo/bar/dog/config.js')]: 'dogMeta',
-  [path.resolve('foo/bar/cat/config.js')]: 'catMeta',
-  [path.resolve('foo/bar/frog/config.js')]: 'frogMeta',
+  [path.resolve('foo/bar/dog/config.js')]: { name:'dogMeta', filePathArray:[] },
+  [path.resolve('foo/bar/cat/config.js')]: { name:'catMeta', filePathArray:[] },
+  [path.resolve('foo/bar/frog/config.js')]: { name:'frogMeta', filePathArray:[] },
 
   // /bar/bar/cat does not have a config file
   [path.resolve('bar/bar/dog/index.js')]: 'dogModule',
   [path.resolve('bar/bar/cat/index.js')]: 'catModule',
-  [path.resolve('bar/bar/dog/config.js')]: 'dogMeta',
+  [path.resolve('bar/bar/dog/config.js')]: { name:'dogMeta', filePathArray:[] },
 
   // does not have index file
-  [path.resolve('wee/bar/dog/config.js')]: 'dogMeta',
+  [path.resolve('wee/bar/dog/config.js')]: { name:'dogMeta', filePathArray:[] },
 
 })
 
@@ -39,15 +43,15 @@ test('(External) sketches - loadSketches()', (t) => {
   const expected = {
     dog: {
       Module: 'dogModule',
-      config: 'dogMeta',
+      config: { name:'dogMeta', filePathArray:[] },
     },
     cat: {
       Module: 'catModule',
-      config: 'catMeta',
+      config: { name:'catMeta', filePathArray:[] },
     },
     frog: {
       Module: 'frogModule',
-      config: 'frogMeta',
+      config: { name:'frogMeta', filePathArray:[] },
     },
   }
   const actual = loadSketches('foo/bar')
