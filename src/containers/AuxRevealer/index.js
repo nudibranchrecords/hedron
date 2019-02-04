6c7ac276f507932b1c7ddbaadbff6dda38a25074
Alex Kempton <alex@funwithtriangles.net>
2019-02-04 11:28:38 +0000

    AuxRevealer container

---
+++ src/containers/AuxRevealer/index.js
@@ -0,0 +1,20 @@
+import { connect } from 'react-redux'
+import Revealer from '../../components/Revealer'
+import withDeferRender from '../../utils/withDeferRender'
+import { uiAuxToggleOpen } from '../../store/ui/actions'
+import getIsAuxOpen from '../../selectors/getIsAuxOpen'
+
+const mapStateToProps = (state, ownProps) => ({
+  isOpen: getIsAuxOpen(state, ownProps.auxId),
+})
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  onHeaderClick: () => {
+    dispatch(uiAuxToggleOpen(ownProps.auxId))
+  },
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withDeferRender(Revealer))
