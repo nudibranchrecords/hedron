d859e284157a323d17f17485ee1ddde882cda7e5
Alex Kempton <alex@funwithtriangles.net>
2019-02-04 11:28:38 +0000

    AuxRevealer container

--- src/containers/SketchParam/index.js
+++ src/containers/SketchParam/index.js
@@ -1,26 +1,14 @@
 import { connect } from 'react-redux'
 import SketchParam from '../../components/SketchParam'
 import withDeferRender from '../../utils/withDeferRender'
-import { uiAuxToggleOpen } from '../../store/ui/actions'
-import getIsAuxOpen from '../../selectors/getIsAuxOpen'
-import uiEventEmitter from '../../utils/uiEventEmitter'
 import getNode from '../../selectors/getNode'
 
 const mapStateToProps = (state, ownProps) => ({
-  advancedIsOpen: getIsAuxOpen(state, ownProps.nodeId),
+  auxRevealerId: ownProps.nodeId,
   isHidden: getNode(state, ownProps.nodeId).hidden,
-})
-
-const mapDispatchToProps = (dispatch, ownProps) => ({
-  onAdvancedClick: () => {
-    dispatch(uiAuxToggleOpen(ownProps.nodeId))
-    setTimeout(() => {
-      uiEventEmitter.emit('recalc-param-heights')
-    })
-  },
 })
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps
+  null
 )(withDeferRender(SketchParam))
