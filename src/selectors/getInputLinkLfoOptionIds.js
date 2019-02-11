d25ce70e08ca5a9757c56a681067a99a659beca7
Alex Kempton <alex@funwithtriangles.net>
2019-02-11 19:35:47 +0000

    Only show "seed" LFO option when noise shape is selected

--- src/selectors/getInputLinkLfoOptionIds.js
+++ src/selectors/getInputLinkLfoOptionIds.js
@@ -1,13 +1,34 @@
+import getNodes from './getNodes'
+
 export default (state, linkId) => {
   const link = state.inputLinks[linkId]
 
   if (link.input && link.input.id === 'lfo') {
+    const optionIds = link.lfoOptionIds
+    const optionNodes = getNodes(state, optionIds)
+    const shapeOpt = optionNodes.find(node => node.key === 'shape')
+    const isNoise = shapeOpt.value === 'noise'
+
+    // Only show "rate" option for shots
     if (link.nodeType === 'shot') {
-      return link.lfoOptionIds.filter(id => {
-        return state.nodes[id].key === 'rate'
+      const typesForShot = ['rate']
+      if (isNoise) {
+        // If its noise, also show the "seed" option
+        typesForShot.push('seed')
+      }
+      return optionIds.filter(id => {
+        return typesForShot.includes(state.nodes[id].key)
       })
     } else {
-      return link.lfoOptionIds
+      // For params, show all options if noise
+      if (isNoise) {
+        return optionIds
+      } else {
+        // If its not noise, omit the "seed" option
+        return optionIds.filter(id => {
+          return state.nodes[id].key !== 'seed'
+        })
+      }
     }
   }
 
