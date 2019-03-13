09153da84660bf0fef24f11b74e8f5586d432a52
Alex Kempton <alex@funwithtriangles.net>
2019-03-13 16:12:22 +0000

    NodeInputIcons as container

---
+++ src/components/NodeInputIcons/index.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import theme from '../../utils/theme'
+
+import inputIcon from '../../assets/icons/input.icon.txt'
+import macroIcon from '../../assets/icons/macro.icon.txt'
+import IconComponent from '../Icon'
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  font-size: 0.5rem;
+  cursor: pointer;
+  text-transform: uppercase;
+
+  > span {
+    display: flex;
+  }
+
+  &:hover {
+    color: ${theme.actionColor1};
+    fill: ${theme.actionColor1};
+  }
+`
+
+const Icon = styled(IconComponent)`
+  width: 0.5rem;
+  height: 0.5rem;
+  margin-right: 0.1rem;
+`
+
+const IconInfo = styled.div`
+  display: flex;
+  margin-left: auto;
+
+  > span {
+    display: flex;
+    margin-left: 0.3rem;
+  }
+`
+
+const NodeInputIcons = ({
+  onClick, inputLinkTitle, numInputs, numMacros,
+}) => (
+  <Wrapper onClick={onClick}>
+    {inputLinkTitle && <span><Icon glyph={inputIcon} />{inputLinkTitle}</span>}
+    <IconInfo>
+      {numInputs !== undefined && (<span><Icon glyph={inputIcon} />{numInputs}</span>)}
+      {numMacros !== undefined && (<span><Icon glyph={macroIcon} />{numMacros}</span>)}
+    </IconInfo>
+  </Wrapper>
+)
+
+NodeInputIcons.propTypes = {
+  onClick: PropTypes.func,
+  numInputs: PropTypes.number,
+  numMacros: PropTypes.number,
+  inputLinkTitle: PropTypes.string,
+}
+
+export default NodeInputIcons
--- src/containers/Node/index.js
+++ src/containers/Node/index.js
@@ -1,25 +1,18 @@
 import { connect } from 'react-redux'
 import Node from '../../components/Node'
 import getNode from '../../selectors/getNode'
-import { uSketchNodeOpenedToggle } from '../../store/sketches/actions'
 import getIsNodeOpen from '../../selectors/getIsNodeOpen'
 import getActiveInputsText from '../../selectors/getActiveInputsText'
 import { nodeShotFired } from '../../store/nodes/actions'
-import { uiNodeToggleOpen } from '../../store/ui/actions'
-import { rMacroOpenToggle } from '../../store/macros/actions'
 
 const mapStateToProps = (state, ownProps) => {
-  const node = getNode(state, ownProps.nodeId)
-  const inputLinkIds = node.inputLinkIds
-  const param = state.nodes[ownProps.nodeId]
+  const param = getNode(state, ownProps.nodeId)
   const type = ownProps.type || 'param'
 
   const inputLinkTitle = getActiveInputsText(state, ownProps.nodeId)
 
   return {
     type,
-    numInputs: inputLinkIds.length,
-    numMacros: node.connectedMacroIds.length,
     title: param.title,
     isOpen: getIsNodeOpen(state, ownProps.nodeId, ownProps.panelId),
     inputLinkTitle,
@@ -30,19 +23,6 @@ const mapDispatchToProps = (dispatch, ownProps) => {
   const type = ownProps.type || 'param'
 
   return {
-    onOpenClick: () => {
-      switch (ownProps.panelId) {
-        case 'overview':
-          dispatch(uiNodeToggleOpen(ownProps.nodeId))
-          break
-        case 'macros':
-          dispatch(rMacroOpenToggle(ownProps.nodeId))
-          break
-        case 'sketch':
-        default:
-          dispatch(uSketchNodeOpenedToggle(ownProps.nodeId))
-      }
-    },
     onParamBarClick: type === 'shot'
     ? () => {
       dispatch(nodeShotFired(ownProps.nodeId, ownProps.sketchId, ownProps.shotMethod))
@@ -50,9 +30,9 @@ const mapDispatchToProps = (dispatch, ownProps) => {
   }
 }
 
-const ParamContainer = connect(
+const NodeContainer = connect(
   mapStateToProps,
   mapDispatchToProps
 )(Node)
 
-export default ParamContainer
+export default NodeContainer
