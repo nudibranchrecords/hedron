41d7edce5f90b5f0fa4cd1ca9d2526f0e659a12e
Alex Kempton <alex@funwithtriangles.net>
2019-03-14 00:07:50 +0000

    Panel title items displaying ancestors

--- src/containers/PropertiesPanel/index.js
+++ src/containers/PropertiesPanel/index.js
@@ -1,15 +1,40 @@
 import { connect } from 'react-redux'
 import PropertiesPanel from '../../components/PropertiesPanel'
+import getNodeAncestors from '../../selectors/getNodeAncestors'
+
+import { uSketchNodeOpenedToggle } from '../../store/sketches/actions'
+import { uiNodeToggleOpen } from '../../store/ui/actions'
+import { rMacroOpenToggle } from '../../store/macros/actions'
 
 const mapStateToProps = (state, ownProps) => {
   const node = ownProps.node
+  const titleItems = node ? getNodeAncestors(state, node.id).reverse() : []
+
   return {
     isOpen: node !== undefined,
-    title: node && node.title,
+    titleItems,
     nodeId: node && node.id,
   }
 }
 
+const mapDipatchToProps = (dispatch, ownProps) => ({
+  onTitleItemClick: (nodeId) => {
+    console.log(ownProps.panelId)
+    switch (ownProps.panelId) {
+      case 'overview':
+        dispatch(uiNodeToggleOpen(nodeId))
+        break
+      case 'macros':
+        dispatch(rMacroOpenToggle(nodeId))
+        break
+      case 'sketch':
+      default:
+        dispatch(uSketchNodeOpenedToggle(nodeId))
+    }
+  },
+})
+
 export default connect(
-  mapStateToProps
+  mapStateToProps,
+  mapDipatchToProps
 )(PropertiesPanel)
