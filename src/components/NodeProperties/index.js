71298db851561fa3ebc48f4aded58915afd3f9d0
Alex Kempton <alex@funwithtriangles.net>
2019-03-14 13:25:51 +0000

    Value displayed for sub nodes in properties panel

--- src/components/NodeProperties/index.js
+++ src/components/NodeProperties/index.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import PropTypes from 'prop-types'
 import Revealer from '../../components/Revealer'
 import ParamRange from '../../containers/ParamRange'
+import Control from '../../containers/Control'
 import InputLinkUI from '../../containers/InputLinkUI'
 import theme from '../../utils/theme'
 import MacroProperties from '../../containers/MacroProperties'
@@ -12,10 +13,26 @@ const Top = styled.div`
   padding-bottom: 0.5rem;
   border-bottom: ${theme.lineColor1} 1px dashed;
 `
+const Bar = styled.div`
+  display: flex;
+  margin-bottom: 0.5rem;
+  text-transform: uppercase;
+  font-size: 0.6rem;
+  align-items: center;
 
-const NodeProperties = ({ nodeId, type, advancedIsOpen, onAdvancedClick }) => (
+  > div {
+    width: 30%;
+  }
+
+  > span {
+    margin-right: 0.5rem;
+  }
+`
+
+const NodeProperties = ({ nodeId, type, advancedIsOpen, onAdvancedClick, displayValue }) => (
   <div>
     <Top>
+      { displayValue && <Bar><span>Value:</span><div><Control nodeId={nodeId} theme='panel' /></div></Bar>}
       <InputLinkUI nodeId={nodeId} />
     </Top>
     {
@@ -33,6 +50,7 @@ NodeProperties.propTypes = {
   advancedIsOpen: PropTypes.bool,
   onAdvancedClick: PropTypes.func.isRequired,
   type: PropTypes.string,
+  displayValue: PropTypes.bool,
 }
 
 export default NodeProperties
