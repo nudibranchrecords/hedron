f7f0e5bd8deef62e7bb52ae24d6909ad6564697b
Alex Kempton <alex@funwithtriangles.net>
2019-03-20 12:26:09 +0000

    onChangeAction passed down to NodeSelect component

--- src/components/Control/index.js
+++ src/components/Control/index.js
@@ -3,10 +3,10 @@ import PropTypes from 'prop-types'
 import ParamBar from '../../containers/ParamBar'
 import NodeSelect from '../../containers/NodeSelect'
 
-const Control = ({ nodeId, onParamBarClick, type, theme }) => {
+const Control = ({ nodeId, onParamBarClick, type, theme, onChangeAction }) => {
   switch (type) {
     case 'select':
-      return <NodeSelect nodeId={nodeId} />
+      return <NodeSelect nodeId={nodeId} onChangeAction={onChangeAction} />
     case 'slider':
     default:
       return <ParamBar
@@ -14,13 +14,17 @@ const Control = ({ nodeId, onParamBarClick, type, theme }) => {
         onMouseDown={onParamBarClick}
         type={type}
         theme={theme}
-            />
+      />
   }
 }
 
 Control.propTypes = {
   nodeId: PropTypes.string.isRequired,
   onParamBarClick: PropTypes.func,
+  onChangeAction: PropTypes.shape({
+    type: PropTypes.string.isRequired,
+    payload: PropTypes.object,
+  }),
   type: PropTypes.string,
   theme: PropTypes.string,
 }
