6d7f4f2a8902934cfb992c0973d563fbe6fa09ec
Alex Kempton <alex@funwithtriangles.net>
2019-03-28 13:35:12 +0000

    Improved styling for button and dropdown

--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -1,28 +1,47 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import theme from '../../utils/theme'
 
 import { Manager, Reference, Popper } from 'react-popper'
 
+const Button = styled.div`
+  height: 18px;
+  border: 1px solid ${theme.lineColor2};
+  padding: 0 0.25rem;
+  font-size: 11px;
+  overflow: hidden;
+  background-color: ${theme.bgColorDark3};
+  display: flex;
+  align-items: center;
+`
+
 const Option = styled.div`
   cursor: pointer;
+  padding: 0.25rem 0.5rem;
+
+  &:hover {
+    color: white;
+    background-color: ${theme.bgColorDark2};
+  }
 `
 
 const Dropdown = styled.div`
- background: black;
- padding: 0.5rem;
- visibility: ${props => props.isVisible ? 'visible' : 'hidden'};
- pointer-events: ${props => props.isVisible ? 'default' : 'none'};
- z-index: 10;
+  background: black;
+  visibility: ${props => props.isVisible ? 'visible' : 'hidden'};
+  pointer-events: ${props => props.isVisible ? 'default' : 'none'};
+  z-index: 10;
+  border: 1px solid ${theme.lineColor2};
+  margin: 0.25rem;
 `
 
 const Select = ({ onOpenClick, isOpen, options, onChange, buttonText }) => (
   <Manager>
     <Reference>
       {({ ref }) => (
-        <button type='button' ref={ref} onClick={onOpenClick} onMouseDown={e => e.stopPropagation()}>
+        <Button ref={ref} onClick={onOpenClick} onMouseDown={e => e.stopPropagation()}>
           { buttonText }
-        </button>
+        </Button>
       )}
     </Reference>
 
