481437fa3be4efd0f1930019aec59713ee9c5793
Alex Kempton <alex@funwithtriangles.net>
2019-08-14 15:31:02 +0100

    Fixed flickering issue with reordering

--- src/components/SketchesNav/index.js
+++ src/components/SketchesNav/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import NavItem from '../NavItem'
 import styled from 'styled-components'
 import { SortableContainer, SortableElement } from 'react-sortable-hoc'
+import arrayMove from 'array-move'
 
 const Wrapper = styled.nav`
   margin-bottom: 2rem;
@@ -32,25 +33,57 @@ const SortableList = SortableContainer(({ items, ...props }) =>
   </ul>
 )
 
-const SketchesNav = ({ onSortEnd, ...props }) => (
-  <Wrapper>
-    <SortableList
-      helperClass='is-sorting'
-      pressDelay={300}
-      lockAxis='y'
-      onSortEnd={({ oldIndex, newIndex }) => { onSortEnd(props.sceneId, oldIndex, newIndex) }}
-      {...props}
-    />
-    <NavItem className='last' to={`/scenes/addSketch/${props.sceneId}`}>+</NavItem>
-  </Wrapper>
-)
+// This sketch can't be stateless because of the reordering
+// Reordering works stateless, but with some annoying flickering
+// It's quite a bit of work just to stop a tiny little thing, but it was annoying!
+// https://github.com/clauderic/react-sortable-hoc/issues/280
+class SketchesNav extends React.Component {
+  state = {
+    items: this.props.items,
+    sceneId: this.props.sceneId,
+  }
+
+  // We update both internal and external state onSortEnd
+  handleOnSortEnd = ({ oldIndex, newIndex }) => {
+    this.setState(({ items }) => ({
+      items: arrayMove(items, oldIndex, newIndex),
+    }))
+    // This wont affect the rendering of the component but we need to store the state properly in redux!
+    this.props.onSortEnd(this.props.sceneId, oldIndex, newIndex)
+  }
+
+  // We update the state based on props if the scene has changed or an item has been added/deleted
+  // If only the order has changed, it will just use the internal state (stops the flickering)
+  static getDerivedStateFromProps (props, state) {
+    if (
+      (props.sceneId !== state.sceneId) || // has the scene changed?
+      (props.items && state.items && props.items.length !== state.items.length) // has an item been added or removed?
+    ) {
+      return {
+        items: props.items,
+        sceneId: props.sceneId,
+      }
+    }
+    return null
+  }
+
+  render () {
+    return <Wrapper>
+      <SortableList
+        {...this.props}
+        items={this.state.items}
+        helperClass='is-sorting'
+        pressDelay={300}
+        lockAxis='y'
+        onSortEnd={this.handleOnSortEnd}
+      />
+      <NavItem className='last' to={`/scenes/addSketch/${this.props.sceneId}`}>+</NavItem>
+    </Wrapper>
+  }
+}
 
 SketchesNav.propTypes = {
-  currentSketchId: PropTypes.oneOfType([
-    PropTypes.string, PropTypes.bool,
-  ]).isRequired,
   sceneId: PropTypes.string.isRequired,
-  onNavItemClick: PropTypes.func.isRequired,
   onSortEnd: PropTypes.func.isRequired,
   items: PropTypes.arrayOf(
     PropTypes.shape({
