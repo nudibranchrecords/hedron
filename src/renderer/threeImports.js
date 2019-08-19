1b189dab184af345864e1567a800145d00642661
Alex Kempton <alex@funwithtriangles.net>
2019-08-19 19:26:39 +0100

    Added `initiatePostProcessing ` method

--- src/renderer/threeImports.js
+++ src/renderer/threeImports.js
@@ -2,6 +2,7 @@
 // This keeps the library versions matched and also prevents strange things from happening when two instances of three
 // are running at the same time
 window.THREE = require('three')
+window.POSTPROCESSING = require('postprocessing')
 
 // For convenience, also requiring some common three extras
 require('three/examples/js/loaders/GLTFLoader')
