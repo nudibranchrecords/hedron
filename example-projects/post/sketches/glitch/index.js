26fb5cf3a0abe8d571e3c04841660c9791101f7c
Alex Kempton <alex@funwithtriangles.net>
2019-08-20 13:13:39 +0100

    More controls for glitch effect

--- example-projects/post/sketches/glitch/index.js
+++ example-projects/post/sketches/glitch/index.js
@@ -5,7 +5,6 @@ class Glitch {
     this.rgbShiftEffect = new ChromaticAberrationEffect()
 
     this.glitchEffect = new GlitchEffect({
-      columns: 0,
       chromaticAberrationOffset: this.rgbShiftEffect.offset,
     })
 
@@ -20,8 +19,17 @@ class Glitch {
   }
 
   update (p) {
+    this.glitchEffect.delay.x = p.delayMin * 1000
+    this.glitchEffect.delay.y = p.delayMax * 1000
+
+    this.glitchEffect.duration.x = p.durationMin * 1000
+    this.glitchEffect.duration.y = p.durationMax * 1000
+
     this.glitchEffect.strength.x = p.glitchAmpStrong
     this.glitchEffect.strength.y = p.glitchAmpWeak
+    this.glitchEffect.ratio = p.strongWeakRatio
+
+    this.glitchEffect.uniforms.get('columns').value = p.columns
   }
 }
 
