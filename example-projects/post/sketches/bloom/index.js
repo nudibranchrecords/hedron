f4a9e945a4763dd04498c56642831092497945c5
Alex Kempton <alex@funwithtriangles.net>
2019-08-20 11:06:14 +0100

    Bloom and glitch post effects

---
+++ example-projects/post/sketches/bloom/index.js
@@ -0,0 +1,30 @@
+const { EffectPass, BloomEffect, BlendFunction, KernelSize } = POSTPROCESSING
+
+class Glitch {
+  initiatePostProcessing (composer) {
+    this.bloomEffect = new BloomEffect({
+      blendFunction: BlendFunction.SCREEN,
+      kernelSize: KernelSize.LARGE,
+      useLuminanceFilter: true,
+      luminanceThreshold: 0.825,
+      luminanceSmoothing: 0.075,
+      height: 480,
+    })
+
+    const pass = new EffectPass(null, this.bloomEffect)
+    composer.addPass(pass)
+
+    // Return the pass that needs to be rendered to the screen (if it's the last pass)
+    return pass
+  }
+
+  update (p) {
+    this.bloomEffect.blurPass.scale = p.scale
+    this.bloomEffect.luminanceMaterial.threshold = p.lumThreshold
+    this.bloomEffect.luminanceMaterial.smoothing = p.lumSmoothing
+    this.bloomEffect.blendMode.opacity.value = p.opacity
+  }
+}
+
+module.exports = Glitch
+
