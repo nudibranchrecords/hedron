9d21cacaa09bc700cf97a3a251284dd81c2009a7
Alex Kempton <alex@funwithtriangles.net>
2019-09-08 20:21:01 +0100

    Chagned structure of props due to spread operator causing perf issues

--- src/containers/NodeSelect/index.js
+++ src/containers/NodeSelect/index.js
@@ -7,25 +7,21 @@ const mapStateToProps = (state, ownProps) => {
   const currentOpt = select.options.find(opt => opt.value === select.value)
   return {
     id: `node_${ownProps.nodeId}`,
-    input: {
-      value: currentOpt,
-    },
+    _value: currentOpt,
     options: select.options,
   }
 }
 
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
-    input: {
-      onChange: option => {
-        dispatch(nodeValueUpdate(ownProps.nodeId, option.value, {
-          dontMutate: true,
-        }))
+    _onChange: option => {
+      dispatch(nodeValueUpdate(ownProps.nodeId, option.value, {
+        dontMutate: true,
+      }))
 
-        if (ownProps.onChangeAction) {
-          dispatch(ownProps.onChangeAction)
-        }
-      },
+      if (ownProps.onChangeAction) {
+        dispatch(ownProps.onChangeAction)
+      }
     },
   }
 }
@@ -33,17 +29,19 @@ const mapDispatchToProps = (dispatch, ownProps) => {
 export default connect(
   mapStateToProps,
   mapDispatchToProps,
-  (stateProps, dispatchProps, ownProps) => ({
-    ...ownProps,
-    ...stateProps,
-    ...dispatchProps,
-    // This structure is needed so the component is compatible
-    // with redux-form (redux-form not actually being used with this component though)
-    ...{
-      input: {
-        value: stateProps.input.value,
-        onChange: dispatchProps.input.onChange,
+  (stateProps, dispatchProps, ownProps) => {
+    return {
+      ...ownProps,
+      ...stateProps,
+      ...dispatchProps,
+      ...{
+        // This structure is needed so the component is compatible
+        // with redux-form (redux-form not actually being used with this component though)
+        input: {
+          onChange: dispatchProps._onChange,
+          value: stateProps._value,
+        },
       },
-    },
-  }),
+    }
+  },
 )(Select)
