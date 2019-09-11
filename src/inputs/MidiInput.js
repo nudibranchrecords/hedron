fe2dd611abcbe3b2349f7415c877d9d420d6a48f
Alex Kempton <alex@funwithtriangles.net>
2019-09-11 20:36:10 +0100

    Rehydrating state without any midi device edits, calling processDevices after rehydration instea

--- src/inputs/MidiInput.js
+++ src/inputs/MidiInput.js
@@ -6,81 +6,86 @@ import { newData as teachMidi } from '../utils/getMidiMode'
 import { processMidiData } from '../utils/midiMessage'
 import getConnectedDevice from '../selectors/getConnectedDevice'
 
-export default (store) => {
-  const onMessage = (rawMessage) => {
-    const state = store.getState()
-    const deviceId = rawMessage.target.name
+let store, midiAccess
 
-    const device = getConnectedDevice(state, deviceId)
-    const m = processMidiData(rawMessage.data, device.settings.forceChannel.value)
+const onMessage = (rawMessage) => {
+  const state = store.getState()
+  const deviceId = rawMessage.target.name
 
-    if (m.messageType !== 'timingClock' && m.messageType !== 'noteOff') {
-      store.dispatch(midiMessage(deviceId, {
-        data: rawMessage.data,
-        timeStamp: rawMessage.timeStamp,
-      }))
+  const device = getConnectedDevice(state, deviceId)
+  const m = processMidiData(rawMessage.data, device.settings.forceChannel.value)
 
-      const learning = state.midi.learning
+  if (m.messageType !== 'timingClock' && m.messageType !== 'noteOff') {
+    store.dispatch(midiMessage(deviceId, {
+      data: rawMessage.data,
+      timeStamp: rawMessage.timeStamp,
+    }))
 
-      if (learning) {
-        let controlType
-        const mode = teachMidi(rawMessage.data, m.messageType)
+    const learning = state.midi.learning
 
-        if (mode !== 'learning') {
-          if (mode === 'ignore') {
-            controlType = undefined
-          } else {
-            // abs, rel1, rel2, rel3
-            controlType = mode
-          }
-          store.dispatch(uInputLinkCreate(
-            learning.id,
-            m.id,
-            learning.type,
-            {
-              controlType,
-              channel: m.channel,
-              messageType: m.messageType,
-              noteNum: m.noteNum,
-            }
-          ))
-          store.dispatch(midiStopLearning())
+    if (learning) {
+      let controlType
+      const mode = teachMidi(rawMessage.data, m.messageType)
+
+      if (mode !== 'learning') {
+        if (mode === 'ignore') {
+          controlType = undefined
+        } else {
+          // abs, rel1, rel2, rel3
+          controlType = mode
         }
-      } else {
-        store.dispatch(inputFired(m.id, m.value, {
-          noteOn: m.messageType === 'noteOn',
-          type: 'midi',
-        }))
+        store.dispatch(uInputLinkCreate(
+          learning.id,
+          m.id,
+          learning.type,
+          {
+            controlType,
+            channel: m.channel,
+            messageType: m.messageType,
+            noteNum: m.noteNum,
+          }
+        ))
+        store.dispatch(midiStopLearning())
       }
-    // If no note data, treat as clock
-    } else if (m.messageType === 'timingClock') {
-      // Only dispatch clock pulse if no generated clock
-      if (!state.clock.isGenerated) {
-        store.dispatch(clockPulse())
-      }
+    } else {
+      store.dispatch(inputFired(m.id, m.value, {
+        noteOn: m.messageType === 'noteOn',
+        type: 'midi',
+      }))
+    }
+  // If no note data, treat as clock
+  } else if (m.messageType === 'timingClock') {
+    // Only dispatch clock pulse if no generated clock
+    if (!state.clock.isGenerated) {
+      store.dispatch(clockPulse())
     }
   }
+}
 
-  const processDevices = ports => {
-    const devices = {}
+export const processDevices = () => {
+  const devices = {}
 
-    ports.forEach((entry) => {
-      devices[entry.name] = {
-        title: entry.name,
-        id: entry.name,
-        manufacturer: entry.manufacturer,
-      }
-      entry.onmidimessage = onMessage
-    })
+  midiAccess.inputs.forEach((entry) => {
+    devices[entry.name] = {
+      title: entry.name,
+      id: entry.name,
+      manufacturer: entry.manufacturer,
+    }
+    entry.onmidimessage = onMessage
+  })
 
-    store.dispatch(midiUpdateDevices(devices))
-  }
+  store.dispatch(midiUpdateDevices(devices))
+}
 
-  navigator.requestMIDIAccess().then((midiAccess) => {
-    processDevices(midiAccess.inputs)
+export default (injectedStore) => {
+  store = injectedStore
+
+  navigator.requestMIDIAccess().then((injectedMidiAccess) => {
+    midiAccess = injectedMidiAccess
+    processDevices()
 
     midiAccess.onstatechange = () => {
-      processDevices(midiAccess.inputs)
+      processDevices()
     }
   })
 }
