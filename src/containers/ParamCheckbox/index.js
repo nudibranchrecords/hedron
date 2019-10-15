bebcdf77b66000f1a675ea1b3d645baf2b6d9c22
Alex Kempton <alex@funwithtriangles.net>
2019-10-15 17:36:38 +0100

    Updating checkbox param component on tick

--- src/containers/ParamCheckbox/index.js
+++ src/containers/ParamCheckbox/index.js
@@ -1,14 +1,6 @@
 import { connect } from 'react-redux'
 import ParamCheckbox from '../../components/ParamCheckbox'
 import { nodeValueUpdate } from '../../store/nodes/actions'
-import getNode from '../../selectors/getNode'
-
-const mapStateToProps = (state, ownProps) => {
-  const node = getNode(state, ownProps.nodeId)
-  return {
-    value: node.value,
-  }
-}
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
   onClick: (value) => {
@@ -17,7 +9,7 @@ const mapDispatchToProps = (dispatch, ownProps) => ({
 })
 
 const ParamCheckboxContainer = connect(
-  mapStateToProps,
+  null,
   mapDispatchToProps
 )(ParamCheckbox)
 
