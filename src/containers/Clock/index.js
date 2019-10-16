5770fa94b65b398de9f2370c1a4fa6480c83823e
Alex Kempton <alex@funwithtriangles.net>
2019-10-16 11:01:11 +0100

    Optimised clock component

--- src/containers/Clock/index.js
+++ src/containers/Clock/index.js
@@ -2,14 +2,6 @@ import { connect } from 'react-redux'
 import Clock from '../../components/Clock'
 import { clockReset, clockSnap } from '../../store/clock/actions'
 import { tap } from '../../inputs/GeneratedClock'
-import getClockBpm from '../../selectors/getClockBpm'
-
-const mapStateToProps = (state, ownProps) => ({
-  beat: (state.clock.beat % 4) + 1,
-  bar: (Math.floor(state.clock.beat / 4) % 4) + 1,
-  phrase: (Math.floor(state.clock.beat / 16) % 4) + 1,
-  bpm: getClockBpm(state),
-})
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
   onResetClick: () => { dispatch(clockReset()) },
@@ -20,12 +12,6 @@ const mapDispatchToProps = (dispatch, ownProps) => ({
 })
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
   null,
-  {
-    // We are mutating state of clock
-    // so this means component always updates
-    areStatesEqual: () => false,
-  }
+  mapDispatchToProps,
 )(Clock)
