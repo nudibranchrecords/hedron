b49578a8a0433b8ad9c77e5f0faac7d1fb9563ba
Cale Bradbury <cale.bradbury@prodigy.com>
2019-11-23 14:58:18 -0500

    Added string valueType

---
+++ src/valueTypes/StringValueType/index.js
@@ -0,0 +1,15 @@
+import { BaseValueType } from '../BaseValueType'
+import ParamStringContainer from './container'
+
+export class StringValueType extends BaseValueType {
+  defaultValue = 'text'
+
+  Component = ParamStringContainer
+
+  doesValueMatch(value) {
+    return typeof value === 'string'
+  }
+
+  compatibleInputs = {
+  }
+}
