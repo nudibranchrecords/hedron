5fe76043fc7c7e26cec59074afadaf45696f0415
Alex Kempton <alex@funwithtriangles.net>
2020-01-14 15:33:17 +0100

    Fixed example projects (removed project json files for now)

--- example-projects/simple/sketches/solid/config.js
+++ example-projects/simple/sketches/solid/config.js
@@ -1,20 +1,11 @@
-/** HEDRON TIP **
-  The config.js defines how the sketch file is used by Hedron.
-**/
-
 module.exports = {
-  // Default title when sketch is loaded in (can be changed by user)
   defaultTitle: 'Solid',
-  // Collapsable category for this sketch to be grouped under
   category:'simple',
-  // Params are values between 0 and 1 that can be manipulated by the user
-  // these values are sent to the sketch every frame
-  // e.g. Speed, scale, colour
   params: [
     {
-      key: 'rotSpeedX', // needs to be unique
-      title: 'Rotation Speed X', // should be human
-      defaultValue: 0.5, // must be between 0 and 1
+      key: 'rotSpeedX',
+      title: 'Rotation Speed X',
+      defaultValue: 0.5,
       defaultMin: -1,
       defaultMax: 1,
     },
@@ -74,12 +65,10 @@ module.exports = {
       ],
     },
   ],
-  // Shots are single functions that can fire, as opposed to values that change
-  // e.g. Explosions, Pre-defined animations
   shots: [
     {
-      method: 'randomGeom', // needs to be unique
-      title: 'Random Geom', // should be human
+      method: 'randomGeom',
+      title: 'Random Geom',
     },
   ],
 }
