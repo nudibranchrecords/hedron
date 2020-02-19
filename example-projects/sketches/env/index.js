e95714c65ff02e60eafbe1f3a643c2d60149c3cb
Alex Kempton <alex@funwithtriangles.net>
2020-02-19 22:16:30 +0000

    Updated logo example project

--- example-projects/sketches/env/index.js
+++ example-projects/sketches/env/index.js
@@ -4,11 +4,15 @@ class Env {
   constructor ({ scene }) {
     this.scene = scene
     this.clearColor = new THREE.Color()
+    this.fog = new THREE.FogExp2()
+    scene.fog = this.fog
   }
 
   update ({ params: p }) {
     this.clearColor.setHSL(p.colorH, p.colorS, p.colorL)
     this.scene.background = this.clearColor
+    this.fog.color = this.clearColor
+    this.fog.density = p.fogDensity
   }
 }
 
