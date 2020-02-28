7504ef21b8e7bc7efc018fe097f7f594eed66a7a
Alex Kempton <alex@funwithtriangles.net>
2020-02-28 14:51:41 +0000

    Added console logs to project fix script

--- projectFixScripts/0.5-0.6.js
+++ projectFixScripts/0.5-0.6.js
@@ -13,7 +13,8 @@ fix(data => {
     const node = data.nodes[key]
 
     if (!node.optionIds) {
-      // option Ids now share the same array property
+      // option IDs now share the same array property
+      console.log(`${node.id}: Converting option IDs into one array`)
       node.optionIds = []
       parseOldOptions('lfoOptionIds', node)
       parseOldOptions('midiOptionIds', node)
@@ -23,10 +24,14 @@ fix(data => {
 
     // We now have valueType, default is "float"
     const becomeFloat = ['param', 'macro', 'macroTargetParamLink']
-    if (!node.valueType && becomeFloat.includes(node.type)) node.valueType = 'float'
+    if (!node.valueType && becomeFloat.includes(node.type)) {
+      console.log(`${node.id}: ${node.type} set to "float" valueType by default`)
+      node.valueType = 'float'
+    }
 
     // Shots become shotFloat
     if (node.type === 'shot') {
+      console.log(`${node.id}: added "shotFloat" valueType for shot`)
       node.valueType = 'shotFloat'
     }
 
@@ -34,13 +39,17 @@ fix(data => {
     if (node.type === 'select') {
       delete node.type
       node.valueType = 'enum'
+      console.log(`${node.id}: "select" type becomes "enum" valueType`)
     }
 
     // Convert macro param link start values from "false" to "null"
     if (node.type === 'macro' && node.targetParamLinks) {
       for (let key in node.targetParamLinks) {
         const obj = node.targetParamLinks[key]
-        if (obj.startValue === false) obj.startValue = null
+        if (obj.startValue === false) {
+          console.log(`${node.id}: false macro start value becomes null`)
+          obj.startValue = null
+        }
       }
     }
 
@@ -52,6 +61,7 @@ fix(data => {
       node.action.payload && node.action.payload.type &&
       !node.action.payload.channel
     ) {
+      console.log(`${node.id}: linkableAction U_SCENE_SELECT_CHANNEL payload.type becomes payload.channel`)
       const p = node.action.payload
       const channel = p.type
       delete p.type
@@ -61,6 +71,7 @@ fix(data => {
 
   // Check if new core nodes are missing
   if (data.nodes.areErrorPopupsDisabled === undefined) {
+    console.log(`adding missing areErrorPopupsDisabled core node`)
     data.nodes.areErrorPopupsDisabled = {
       title: 'Disable Error Popups',
       id: 'areErrorPopupsDisabled',
@@ -68,6 +79,8 @@ fix(data => {
       value: false,
     }
   }
+
+  console.log(`Checked ${Object.keys(data.nodes).length} nodes`)
 
   return data
 })
