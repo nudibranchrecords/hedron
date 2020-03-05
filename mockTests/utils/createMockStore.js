79c54ead00ecb84527547243a8e59e8865abc650
Alex Kempton <alex@funwithtriangles.net>
2020-03-05 12:19:59 +0000

    Added createMockStore test util

---
+++ mockTests/utils/createMockStore.js
@@ -0,0 +1,26 @@
+/** * SETUP ***/
+
+import listen from 'redux-action-listeners'
+import { createStore, applyMiddleware, combineReducers } from 'redux'
+
+import createSagaMiddleware from 'redux-saga'
+const sagaMiddleware = createSagaMiddleware()
+
+export const createMockStore = ({ startState, reducers, rootSaga, listeners }) => {
+  const rootReducer = combineReducers(reducers)
+
+  const rootListener = {
+    types: 'all',
+
+    handleAction (action, dispatched, store) {
+      listeners.forEach(listener => {
+        listener(action, store)
+      })
+    },
+  }
+
+  const store = createStore(rootReducer, startState, applyMiddleware(sagaMiddleware, listen(rootListener)))
+  sagaMiddleware.run(rootSaga, store.dispatch)
+
+  return { store, startState }
+}
