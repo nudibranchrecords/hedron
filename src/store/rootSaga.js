272ed981bc7ac09045808656f229360ae1d76876
Alex Kempton <alex@funwithtriangles.net>
2020-03-05 12:43:01 +0000

    Converted inputlink sagas to listeners

--- src/store/rootSaga.js
+++ src/store/rootSaga.js
@@ -1,12 +1,10 @@
 import { fork } from 'redux-saga/effects'
-import { watchInputLinks } from './inputLinks/sagas'
 import { watchClock } from './clock/sagas'
 import { watchWindows } from './windows/sagas'
 import { watchMacros } from './macros/sagas'
 
 export default function* rootSaga (dispatch) {
   yield [
-    fork(watchInputLinks),
     fork(watchClock),
     fork(watchWindows),
     fork(watchMacros),
