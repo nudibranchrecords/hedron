b0b71e46a7182be811a979d8a2e462e9d2c1f1d8
Alex Kempton <alex@funwithtriangles.net>
2024-08-23 16:35:19 +0200

    displaying output on second window

--- src/renderer/engine/renderer.ts
+++ src/renderer/engine/renderer.ts
@@ -6,6 +6,12 @@ import { engineScenes } from '.'
 // https://github.com/microsoft/TypeScript/pull/55887
 let renderer: WebGLRenderer | undefined
 let viewerEl: HTMLDivElement | undefined
+let rendererHeight: number
+let rendererWidth: number
+let outputEl: HTMLDivElement | undefined | null
+let previewCanvas: HTMLCanvasElement | undefined
+let outputCanvas: HTMLCanvasElement | undefined
+let domEl: HTMLCanvasElement | undefined
 
 const isSendingOutput = false
 
@@ -14,7 +20,7 @@ export const createCanvas = (containerEl: HTMLDivElement): void => {
     antialias: false, // antialiasing should be handled by the composer
   })
 
-  const domEl = renderer.domElement
+  domEl = renderer.domElement
 
   containerEl.innerHTML = ''
   containerEl.appendChild(domEl)
@@ -63,14 +69,49 @@ export const setSize = (): void => {
     scene.setRatio(ratio)
   })
 
-  // rendererWidth = width
-  // rendererHeight = height
+  rendererWidth = width
+  rendererHeight = height
 
   // CSS trick to resize canvas
   viewerEl.style.paddingBottom = perc + '%'
 }
 
-export const setOutput = (win: Window): void => {}
+export const setOutput = (win: Window): void => {
+  stopOutput()
+  const container = win.document.querySelector('div')
+
+  rendererHeight = container.offsetWidth
+  rendererWidth = container.offsetHeight
+  outputEl = container
+
+  // Move renderer canvas to new window
+  outputEl.appendChild(domEl)
+  domEl.setAttribute('style', '')
+
+  // Setup output canvas
+  // If preview and output are different, this canvas will be used and
+  // renderer renders two different images, with images being copied from
+  // the dom element to both preview and output canvases
+  outputCanvas = document.createElement('canvas')
+  outputCanvas.style = 'position: absolute; left: 0; top:0; height: 0; width:100%; height:100%;'
+  outputContext = outputCanvas.getContext('2d')
+  outputEl.appendChild(outputCanvas)
+
+  // Setup preview canvas in dom
+  // Pixels will be copied from renderer dom element to this
+  previewCanvas = document.createElement('canvas')
+  previewCanvas.style = 'position: absolute; left: 0; top:0; height: 0; width:100%; height:100%;'
+  previewContext = previewCanvas.getContext('2d')
+  viewerEl.appendChild(previewCanvas)
+
+  isSendingOutput = true
+
+  setSize()
+
+  win.addEventListener('resize', () => {
+    uiEventEmitter.emit('repaint')
+  })
+}
 
 export const stopOutput = () => {}
 
