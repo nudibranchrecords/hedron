2174347dc01fb0d14189f8a84aae49fe33143b29
Alex Kempton <alex@funwithtriangles.net>
2024-08-24 11:25:54 +0200

    tidy

--- src/renderer/engine/renderer.ts
+++ src/renderer/engine/renderer.ts
@@ -5,15 +5,13 @@ import { engineScenes } from '.'
 // TODO: typing with undefined should be enforced with new TS setting soon
 // https://github.com/microsoft/TypeScript/pull/55887
 let renderer: WebGLRenderer | undefined
-let viewerEl: HTMLDivElement | undefined
 let rendererHeight: number
 let rendererWidth: number
-let outputEl: HTMLDivElement | undefined | null
+let viewerContainer: HTMLDivElement | undefined
+let outputContainer: HTMLDivElement | undefined | null
 let previewCanvas: HTMLCanvasElement | undefined
 let outputCanvas: HTMLCanvasElement | undefined
 let canvas: HTMLCanvasElement | undefined
-
-const canvasStyle = 'position: absolute; left: 0; top:0; height: 0; width:100%; height:100%;'
 
 let isSendingOutput = false
 
@@ -26,14 +24,14 @@ export const createCanvas = (containerEl: HTMLDivElement): void => {
 
   containerEl.innerHTML = ''
   containerEl.appendChild(canvas)
-  viewerEl = containerEl
+  viewerContainer = containerEl
 
   setSize()
 }
 
 export const setSize = (): void => {
   if (!renderer) throw new Error('Renderer not set')
-  if (!viewerEl) throw new Error('viewerEl not set')
+  if (!viewerContainer) throw new Error('viewerEl not set')
 
   // TODO: settings in state
   const settings = {
@@ -46,16 +44,18 @@ export const setSize = (): void => {
   if (isSendingOutput) {
     if (!previewCanvas) throw new Error('previewCanvas not set')
     if (!outputCanvas) throw new Error('outputCanvas not set')
+    if (!outputContainer) throw new Error('outputContainer not set')
+
     // Get width and ratio from output window
-    width = outputEl.offsetWidth
-    ratio = width / outputEl.offsetHeight
+    width = outputContainer.offsetWidth
+    ratio = width / outputContainer.offsetHeight
     previewCanvas.width = width
     previewCanvas.height = width / ratio
     outputCanvas.width = width
     outputCanvas.height = width / ratio
   } else {
     // Basic width and ratio if no output
-    width = viewerEl.offsetWidth
+    width = viewerContainer.offsetWidth
     ratio = settings.aspectW / settings.aspectH
   }
 
@@ -75,22 +75,22 @@ export const setSize = (): void => {
   rendererHeight = height
 
   // CSS trick to resize canvas
-  viewerEl.style.paddingBottom = perc + '%'
+  viewerContainer.style.paddingBottom = perc + '%'
 }
 
 export const setOutput = (win: Window): void => {
   stopOutput()
-  const container = win.document.querySelector('div')
+  outputContainer = win.document.querySelector('div')
 
-  if (!container) throw new Error("Can't find container")
+  if (!outputContainer) throw new Error("Can't find container")
   if (!canvas) throw new Error("Can't find canvas")
+  if (!viewerContainer) throw new Error("Can't find vie")
 
-  rendererHeight = container.offsetWidth
-  rendererWidth = container.offsetHeight
-  outputEl = container
+  rendererHeight = outputContainer.offsetWidth
+  rendererWidth = outputContainer.offsetHeight
 
   // Move renderer canvas to new window
-  outputEl.appendChild(canvas)
+  outputContainer.appendChild(canvas)
   canvas.setAttribute('style', '')
 
   // Setup output canvas
@@ -98,16 +98,14 @@ export const setOutput = (win: Window): void => {
   // renderer renders two different images, with images being copied from
   // the dom element to both preview and output canvases
   outputCanvas = document.createElement('canvas')
-  // outputCanvas.setAttribute('style', canvasStyle)
   // outputContext = outputCanvas.getContext('2d')
-  outputEl.appendChild(outputCanvas)
+  outputContainer.appendChild(outputCanvas)
 
   // Setup preview canvas in dom
   // Pixels will be copied from renderer dom element to this
   previewCanvas = document.createElement('canvas')
-  // previewCanvas.setAttribute('style', canvasStyle)
   // previewContext = previewCanvas.getContext('2d')
-  viewerEl.appendChild(previewCanvas)
+  viewerContainer.appendChild(previewCanvas)
 
   isSendingOutput = true
 
