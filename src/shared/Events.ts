ed316b95b32a65e561960bf5e527565a126d87d7
cale bradbury <cale.bradbury@gmail.com>
2024-08-27 15:47:47 -0400

    Moved magic strings to enums

---
+++ src/shared/Events.ts
@@ -0,0 +1,12 @@
+export enum SketchEvents {
+    NewSketch = 'new-sketch',
+}
+
+export enum ScreenEvents {
+    SendOutput = 'send-output',
+    UpdateDisplays = 'update-displays',
+}
+
+export enum UIEvents {
+    Repaint = 'repaint',
+}
