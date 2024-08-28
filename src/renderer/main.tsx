bbb342039d6e31f8ac74c8048770eda6cb0291a8
Alex Kempton <alex@funwithtriangles.net>
2024-08-28 17:33:17 +0200

    tidy

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -2,19 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './components/App'
 import './base.css'
-import { run } from './engine'
+
 import './windows'
 import './globalVars'
-import { SketchEvents } from '../shared/Events'
-import { refreshSketch } from './engine/sketches'
-
-window.electron.ipcRenderer.on(SketchEvents.ServerStart, (_, sketchesServerUrl) => {
-  run(sketchesServerUrl)
-})
-
-window.electron.ipcRenderer.on(SketchEvents.RefreshSketch, (_, sketchId) => {
-  refreshSketch(sketchId)
-})
+import './mainThreadListen'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
