5f7b200b363d725adb521c3ca3e30db65cf9820c
Alex Kempton <alex@funwithtriangles.net>
2024-08-29 16:26:19 +0200

    refactor

--- src/renderer/engine/index.ts
+++ src/renderer/engine/index.ts
@@ -1,11 +1,8 @@
 import { render } from './renderer'
 import { createDebugScene } from './debugScene'
-import { setSketchesServerUrl } from './globals'
 import { loadSketch } from './sketches'
 
-export const run = async (sketchesServerUrl: string): Promise<void> => {
-  setSketchesServerUrl(sketchesServerUrl)
-
+export const run = async (): Promise<void> => {
   const debugScene = createDebugScene()
 
   // TODO: Actually load in sketches from state!
