2dd27bb6e2f50a1c91587a6566fb37c41d133302
Alex Kempton <alex@funwithtriangles.net>
2024-08-31 15:37:37 +0200

    add sketch to state

--- src/renderer/components/WorkArea/index.tsx
+++ src/renderer/components/WorkArea/index.tsx
@@ -1,28 +1,36 @@
+import { useMemo } from 'react'
 import { useAppStore } from 'src/renderer/engine/sketchesState'
 
 export const WorkArea = (): JSX.Element => {
   const sketchLibrary = useAppStore((state) => state.sketchLibrary)
   const sketches = useAppStore((state) => state.sketches)
+  const addSketch = useAppStore((state) => state.addSketch)
 
   // TODO: Proper selectors/hooks to get this stuff
-  const sketchLibraryVals = Object.values(sketchLibrary)
-  const sketchesVals = Object.values(sketches)
+  const sketchLibraryVals = useMemo(() => Object.values(sketchLibrary), [sketchLibrary])
+  const sketchesVals = useMemo(() => Object.values(sketches), [sketches])
+
+  console.log(sketchLibrary)
+
+  console.log(sketchLibraryVals)
 
   return (
     <div>
       <h2>Add Sketches</h2>
       <ul>
-        {sketchLibraryVals.map(({ name }) => (
-          <li key={name}>
-            <button>{name}</button>
+        {sketchLibraryVals.map(({ name, sketchId }) => (
+          <li key={sketchId}>
+            <button onClick={() => addSketch(sketchId)}>{name}</button>
           </li>
         ))}
       </ul>
       <h2>Remove sketches</h2>
       <ul>
-        {sketchesVals.map(({ sketchId }) => (
-          <li key={sketchId}>
-            <button>Remove {sketchId}</button>
+        {sketchesVals.map(({ sketchId, id }) => (
+          <li key={id}>
+            <button>
+              Remove {sketchId} ({id})
+            </button>
           </li>
         ))}
       </ul>
