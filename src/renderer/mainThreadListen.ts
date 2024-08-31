35d7610b73e9ba217bfb89a80cc1e939266aee1d
Alex Kempton <alex@funwithtriangles.net>
2024-08-31 14:05:18 +0200

    sketchLibrary in store

--- src/renderer/mainThreadListen.ts
+++ src/renderer/mainThreadListen.ts
@@ -1,23 +1,45 @@
 import { SketchEvents } from '../shared/Events'
 import { refreshSketch } from './engine/sketches'
 import { run } from './engine'
-import { setSketchesServerUrl } from './engine/globals'
+import { getSketchesServerUrl, setSketchesServerUrl } from './engine/globals'
+import { SketchLibrary, useAppStore } from './engine/sketchesState'
+import { uid } from 'uid'
 
-const listen = (event: string, cb: (info: string) => void): void => {
+const listen = (event: string, cb: (info: any) => void): void => {
   window.electron.ipcRenderer.on(event, (_, info) => {
     cb(info)
   })
 }
 
-listen(SketchEvents.InitialSketchIds, (sketchIds) => {
-  console.log(sketchIds)
+listen(SketchEvents.InitialSketchIds, (sketchIds: string[]) => {
+  const base = getSketchesServerUrl()
+  const cacheBust = uid()
+
+  const getSketchInfo = async (): Promise<void> => {
+    const sketchLibrary: SketchLibrary = {}
+    for (const sketchId of sketchIds) {
+      console.log(sketchId)
+      const module = await import(/* @vite-ignore */ `${base}/${sketchId}/config.js?${cacheBust}`)
+
+      const name = module.default.defaultTitle
+
+      sketchLibrary[sketchId] = {
+        sketchId,
+        name,
+      }
+
+      useAppStore.getState().setSketchLibrary(sketchLibrary)
+    }
+  }
+
+  getSketchInfo()
 })
 
-listen(SketchEvents.ServerStart, (sketchesServerUrl) => {
+listen(SketchEvents.ServerStart, (sketchesServerUrl: string) => {
   setSketchesServerUrl(sketchesServerUrl)
   run()
 })
 
-listen(SketchEvents.RefreshSketch, (sketchId) => {
+listen(SketchEvents.RefreshSketch, (sketchId: string) => {
   refreshSketch(sketchId)
 })
