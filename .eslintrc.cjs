1c6c6d7723dab977dbe2a4bcaa5e989842aaa400
Alex Kempton <alex@funwithtriangles.net>
2024-09-02 17:04:52 +0200

    ts fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,6 +4,9 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     '@electron-toolkit/eslint-config-ts/recommended',
-    '@electron-toolkit/eslint-config-prettier'
-  ]
+    '@electron-toolkit/eslint-config-prettier',
+  ],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 0,
+  },
 }
