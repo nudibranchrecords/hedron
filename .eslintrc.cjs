a648a78dc34f7a9bd0784387dfdfec8f85e0825e
Alex Kempton <alex@funwithtriangles.net>
2024-09-03 16:38:06 +0200

    storybook with button and vars

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,5 @@
 module.exports = {
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    '@electron-toolkit/eslint-config-ts/recommended',
-    '@electron-toolkit/eslint-config-prettier',
-  ],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react/jsx-runtime', '@electron-toolkit/eslint-config-ts/recommended', '@electron-toolkit/eslint-config-prettier', 'plugin:storybook/recommended'],
   rules: {
     '@typescript-eslint/explicit-function-return-type': 0,
   },
---
+++ src/renderer/components/core/Button/index.tsx
@@ -0,0 +1,24 @@
+import c from './styles.module.css'
+
+export interface ButtonProps {
+  type?: 'primary' | 'secondary'
+  children: React.ReactNode
+  onClick?: () => void
+}
+
+export const Button = ({
+  type = 'primary',
+  children,
+  ...props
+}: ButtonProps) => {
+
+  return (
+    <button
+      type="button"
+      className={`${c.wrapper} ${type}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
