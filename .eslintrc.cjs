ecb6fd832a6cc861d297047db22447ba6db33c7d
Alex Kempton <alex@funwithtriangles.net>
2024-09-04 16:36:45 +0200

    eslint plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,13 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react/jsx-runtime', '@electron-toolkit/eslint-config-ts/recommended', '@electron-toolkit/eslint-config-prettier', 'plugin:storybook/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    '@electron-toolkit/eslint-config-ts/recommended',
+    '@electron-toolkit/eslint-config-prettier',
+    'plugin:storybook/recommended',
+    'plugin:react-hooks/recommended',
+  ],
   rules: {
     '@typescript-eslint/explicit-function-return-type': 0,
   },
