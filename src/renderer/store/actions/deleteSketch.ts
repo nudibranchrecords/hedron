0ffa19d07a2f9cd534049793aa080651a03b01d1
Alex Kempton <alex@funwithtriangles.net>
2024-09-05 18:45:10 +0200

    delete nodeValues when sketch deleted

--- src/renderer/store/actions/deleteSketch.ts
+++ src/renderer/store/actions/deleteSketch.ts
@@ -4,6 +4,7 @@ export const deleteSketch = (instanceId: string) => {
   useAppStore.setState((state) => {
     state.sketches[instanceId].paramIds.forEach((paramId) => {
       delete state.nodes[paramId]
+      delete state.nodeValues[paramId]
     })
 
     delete state.sketches[instanceId]
