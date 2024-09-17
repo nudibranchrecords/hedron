692bf4113092d7f124bebcfd7074b3d2530e1e11
Alex Kempton <alex@funwithtriangles.net>
2024-09-17 16:31:08 +0200

    node param names update with config

--- src/renderer/components/hooks/useActiveSketchParams.ts
+++ src/renderer/components/hooks/useActiveSketchParams.ts
@@ -1,18 +1,27 @@
 import { useMemo } from 'react'
 import { useActiveSketch } from './useActiveSketch'
 import { useEngineStore } from '../../engine'
+import { Param } from 'src/engine/store/types'
+
+export type ParamWithInfo = Param & { title: string | undefined }
 
 export const useActiveSketchParams = () => {
   const activeSketch = useActiveSketch()
   const nodes = useEngineStore((state) => state.nodes)
+  const module = useEngineStore(
+    (state) => activeSketch && state.sketchModules[activeSketch.moduleId],
+  )
 
-  const params = useMemo(() => {
+  const params: ParamWithInfo[] = useMemo(() => {
     if (activeSketch) {
-      return activeSketch.paramIds.map((id) => nodes[id])
+      return activeSketch.paramIds.map((id, index) => {
+        const node = nodes[id]
+        const title = module?.config.params[index].title
+        return { ...node, title }
+      })
     }
-
     return []
-  }, [nodes, activeSketch])
+  }, [activeSketch, nodes, module])
 
   return params
 }
