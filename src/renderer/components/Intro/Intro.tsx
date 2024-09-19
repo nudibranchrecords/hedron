d98092642ea6ccb52a91e2da170895bf6f5a6eda
Alex Kempton <alex@funwithtriangles.net>
2024-09-19 17:28:13 +0200

    layout tweaks

--- src/renderer/components/Intro/Intro.tsx
+++ src/renderer/components/Intro/Intro.tsx
@@ -2,6 +2,7 @@ import { useCallback } from 'react'
 import c from './Intro.module.css'
 import { Button } from '../core/Button/Button'
 import { FileEvents } from 'src/shared/Events'
+import { Panel, PanelActions, PanelBody, PanelHeader } from '../core/Panel/Panel'
 
 export const Intro = () => {
   const onButtonClick = useCallback(() => {
@@ -10,8 +11,13 @@ export const Intro = () => {
 
   return (
     <div className={c.wrapper}>
-      <p>Choose your sketches folder...</p>
-      <Button onClick={onButtonClick}>Select Sketches Folder</Button>
+      <Panel>
+        <PanelHeader>Welcome back</PanelHeader>
+        <PanelBody>Choose your sketches folder or open a project to get started</PanelBody>
+        <PanelActions>
+          <Button onClick={onButtonClick}>Select Sketches Folder</Button>
+        </PanelActions>
+      </Panel>
     </div>
   )
 }
