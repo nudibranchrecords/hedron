cf8fdab9e65db3f6a734845782496d0c30b61f4e
Alex Kempton <alex@funwithtriangles.net>
2024-09-19 15:53:15 +0200

    panel component

---
+++ src/stories/Panel.stories.tsx
@@ -0,0 +1,38 @@
+import type { Meta } from '@storybook/react'
+import {
+  Panel,
+  PanelActions,
+  PanelBody,
+  PanelHeader,
+} from '../renderer/components/core/Panel/Panel'
+import { fn } from '@storybook/test'
+import { Button } from '../renderer/components/core/Button/Button'
+
+const meta = {
+  title: 'Panel',
+  component: Panel,
+  parameters: {},
+} satisfies Meta<typeof Panel>
+
+export default meta
+
+export const WithActions = () => {
+  return (
+    <Panel>
+      <PanelHeader>Welcome to the panel!</PanelHeader>
+      <PanelBody>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam recusandae voluptatum
+          quidem minus atque, numquam explicabo blanditiis ad corporis eligendi delectus, incidunt
+          ipsum harum error. Quidem dolorem exercitationem nostrum dignissimos!
+        </p>
+      </PanelBody>
+      <PanelActions>
+        <Button onClick={fn()}>Do something</Button>
+        <Button type="secondary" onClick={fn()}>
+          Do something else
+        </Button>
+      </PanelActions>
+    </Panel>
+  )
+}
