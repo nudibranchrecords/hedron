d219138cc8b6434a1514503418c24f097902a8ac
cale bradbury <cale.bradbury@gmail.com>
2024-09-24 22:06:38 -0400

    try instancing the sketch and calling getConfig to get the config object

--- src/engine/importSketchModule.ts
+++ src/engine/importSketchModule.ts
@@ -7,14 +7,31 @@ export const importSketchModule = async (
 ): Promise<SketchModuleItem> => {
   const cacheBust = uid()
 
-  const configPath = `${baseUrl}/${moduleId}/config.js?${cacheBust}`
+  // Get the sketch module
   const sketchPath = `${baseUrl}/${moduleId}/index.js?${cacheBust}`
-
-  const configModule = await import(/* @vite-ignore */ configPath)
+  if ((await fetch(sketchPath)).status !== 200) {
+    return Promise.reject(`Sketch module not found: ${sketchPath}`)
+  }
   const sketchModule = await import(/* @vite-ignore */ sketchPath)
-
-  const config: SketchConfig = configModule.default
   const module: SketchModule = sketchModule.default
+
+  // Get the sketch config
+  const configPath = `${baseUrl}/${moduleId}/config.js?${cacheBust}`
+  let config: SketchConfig
+  if ((await fetch(configPath)).status !== 200) {
+    // No config file found
+    // Try instancing the sketch, and call getConfig() on it
+    const tempModule = new module()
+    config = tempModule.getConfig?.()
+    if (!config) {
+      return Promise.reject(
+        `Sketch config not found: ${configPath} and no valid getConfig() function found in sketch`,
+      )
+    }
+  } else {
+    const configModule = await import(/* @vite-ignore */ configPath)
+    config = configModule.default
+  }
 
   // A config could be missing a title, but it is a required parameter
   if (!config.title) {
