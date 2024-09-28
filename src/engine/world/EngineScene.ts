5b4301e870b8cd4dc0de133b0b2c049160a6907f
cale bradbury <cale.bradbury@gmail.com>
2024-09-28 03:33:32 -0400

    Add render pass array to engine scene

--- src/engine/world/EngineScene.ts
+++ src/engine/world/EngineScene.ts
@@ -1,17 +1,30 @@
-import { PerspectiveCamera, Scene } from 'three'
+import { Pass, RenderPass } from 'postprocessing'
+import { PerspectiveCamera, Scene, WebGLRenderer } from 'three'
 
 export class EngineScene {
   public scene: Scene
   public camera: PerspectiveCamera
+  public passes: Pass[]
+  public renderer: WebGLRenderer
 
   constructor() {
     this.scene = new Scene()
     this.camera = new PerspectiveCamera(75, undefined, 0.1, 100000)
     this.camera.position.z = 5
+    const renderPass = new RenderPass(this.scene, this.camera)
+    this.passes = [renderPass]
   }
 
   setRatio(ratio: number): void {
     this.camera.aspect = ratio
     this.camera.updateProjectionMatrix()
+  }
+
+  addPass(pass: Pass): void {
+    this.passes.push(pass)
+  }
+
+  clearPasses(): void {
+    this.passes = [this.passes[0]]
   }
 }
