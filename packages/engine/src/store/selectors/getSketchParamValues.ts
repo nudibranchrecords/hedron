b7e76668fbee053063b81c00f736b11c38a2ca3b
Alex Kempton <alex@funwithtriangles.net>
2024-12-18 15:01:50 +0100

    comments

--- packages/engine/src/store/selectors/getSketchParamValues.ts
+++ packages/engine/src/store/selectors/getSketchParamValues.ts
@@ -1,5 +1,6 @@
 import { EngineState } from '@store/types'
 
+// Get the values of the parameters of a sketch, dealing with child nodes
 export const getSketchParamValues = (state: EngineState, sketchId: string) => {
   const { sketches, nodeValues, nodes } = state
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -12,6 +13,7 @@ export const getSketchParamValues = (state: EngineState, sketchId: string) => {
     let value
 
     if (valueType === 'vector3') {
+      // Return an array of values for vector3
       const childNodeIds = nodes[id].childNodeIds
       value = childNodeIds.map((childNodeId) => nodeValues[childNodeId])
     } else {
