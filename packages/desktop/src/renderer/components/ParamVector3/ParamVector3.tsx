a5516001e7e35596f4e94b73cd28cae4c38b9e13
Alex Kempton <alex@funwithtriangles.net>
2024-12-18 14:30:34 +0100

    ParamVector3 component

---
+++ packages/desktop/src/renderer/components/ParamVector3/ParamVector3.tsx
@@ -0,0 +1,42 @@
+import { useRef } from 'react'
+import { useInterval } from 'usehooks-ts'
+import { NodeParamVector3 } from '@hedron/engine'
+import c from './ParamVector3.module.css'
+import { FloatSlider, FloatSliderHandle } from '@components/core/FloatSlider/FloatSlider'
+import { useOnNodeValueChange } from '@components/hooks/useOnNodeValueChange'
+import { engineStore, useEngineStore } from '@renderer/engine'
+
+interface ParamVector3Props {
+  id: string
+}
+
+const SingleSlider = ({ id }: ParamVector3Props) => {
+  const ref = useRef<FloatSliderHandle>(null)
+  const onValueChange = useOnNodeValueChange(id)
+
+  useInterval(() => {
+    const nodeValue = engineStore.getState().nodeValues[id]
+    if (typeof nodeValue !== 'number') {
+      throw new Error('VectorComponentSlider value was not a number')
+    }
+    ref.current?.drawBar(nodeValue)
+  }, 100)
+
+  return <FloatSlider ref={ref} onValueChange={onValueChange} />
+}
+
+export const ParamVector3 = ({ id }: ParamVector3Props) => {
+  const node = useEngineStore((state) => state.nodes[id] as NodeParamVector3)
+
+  const { childNodeIds } = node
+
+  return (
+    <div className={c.container}>
+      {childNodeIds.map((childId) => (
+        <div key={childId} className={c.item}>
+          <SingleSlider key={childId} id={childId} />
+        </div>
+      ))}
+    </div>
+  )
+}
