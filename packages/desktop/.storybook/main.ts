dee7e902a7c17a3c2590178dbf4c5454885e82b3
Alex Kempton <alex@funwithtriangles.net>
2025-01-03 15:45:13 +0100

    Bottom panel (#480)
    * storybook fix
    
    * BottomPanel story
    
    * NodeControl color variant
    
    * ts fix
    
    * using css vars instead of props
    
    * displaying panel, ParamWithInfo
    
    * tidy
    
    * bottom panel styling

--- packages/desktop/.storybook/main.ts
+++ packages/desktop/.storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from '@storybook/react-vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -12,6 +13,13 @@ const config: StorybookConfig = {
   framework: {
     name: '@storybook/react-vite',
     options: {},
+  },
+  viteFinal: async (config) => {
+    // @ts-expect-error
+    config.plugins = [...config.plugins, tsconfigPaths()]
+    return {
+      ...config,
+    }
   },
 }
 export default config
