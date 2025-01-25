daa9884c441706d8ce6aae47dccda7fcfdfeb43b
Alex Kempton <alex@funwithtriangles.net>
2025-01-25 14:31:06 +0100

    test app extras

--- packages/clock/clock-test-app/src/App.tsx
+++ packages/clock/clock-test-app/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { FormEvent, useEffect } from 'react'
 // eslint-disable-next-line
 import { Clock } from '../../src/Clock'
 import './custom.css'
@@ -10,6 +10,10 @@ const H = 300 - 4
 
 const TAU = Math.PI * 2
 
+const $text = (id: string, text: string | number) => {
+  document.querySelector<HTMLDivElement>(`#${id}`)!.textContent = text.toString()
+}
+
 const $y = (id: string, y: number) =>
   (document.querySelector<HTMLDivElement>(`#${id}`)!.style.transform = `translateY(${y}px)`)
 
@@ -18,8 +22,9 @@ function App() {
     const update = () => {
       const d = Math.round(clock.beatDelta * 1000) / 1000
 
-      document.querySelector('#delta')!.textContent = d.toString()
-      document.querySelector('#beat')!.textContent = clock.beatCount.toString()
+      $text('delta', d)
+      $text('bpm', clock.bpm)
+      $text('beat', clock.beatCount)
 
       $y('saw', (d * H) % H)
       $y('sin', Math.sin(d * TAU) * H * 0.5 + H * 0.5)
@@ -33,14 +38,23 @@ function App() {
     requestAnimationFrame(update)
   }, [])
 
+  const onBpmSubmit = (e: FormEvent) => {
+    e.preventDefault()
+    clock.bpm = Number(document.querySelector<HTMLInputElement>('#bpmField')!.value)
+  }
+
   return (
     <>
       <section>
         <div className="grid">
-          <code id="delta"></code>
           <button onClick={clock.start}>start</button>
           <button onClick={clock.stop}>stop</button>
           <button onClick={clock.reset}>reset</button>
+          <div>
+            <code>
+              Delta: <span id="delta"></span>
+            </code>
+          </div>
         </div>
       </section>
       <section>
@@ -52,9 +66,28 @@ function App() {
           <div id="triangle" className="pip"></div>
         </div>
       </section>
-      <div className="grid">
-        <code id="beat"></code>
-      </div>
+
+      <section className="grid">
+        <div>
+          <div>Current Beat</div>
+          <h3 id="beat"></h3>
+        </div>
+        <div>
+          <div>Current BPM</div>
+          <h3 id="bpm"></h3>
+        </div>
+        <div>
+          <label>Set BPM</label>
+          <form className="grid" onSubmit={onBpmSubmit}>
+            <div>
+              <input id="bpmField" type="number"></input>
+            </div>
+            <div>
+              <button type="submit">submit</button>
+            </div>
+          </form>
+        </div>
+      </section>
     </>
   )
 }
