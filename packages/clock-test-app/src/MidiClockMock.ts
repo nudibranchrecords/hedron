eb2cace55bc7f4bb6599e90905ae022c90083559
Alex Kempton <alex@funwithtriangles.net>
2025-01-26 18:48:37 +0100

    trying to increase accuracy of mock clock

--- packages/clock-test-app/src/MidiClockMock.ts
+++ packages/clock-test-app/src/MidiClockMock.ts
@@ -4,7 +4,8 @@ const MS_IN_MINUTE = 60000
 export class MidiClockMock {
   private _bpm: number = 0
   private pulseCallback: (() => void) | null = null
-  private interval: number = 0
+  private _pulseInterval: number = 0
+  private _lastPulseTimestamp: number | null = null
   public isEnabled = false
 
   constructor(bpm: number) {
@@ -18,7 +19,7 @@ export class MidiClockMock {
 
   set bpm(value: number) {
     this._bpm = value
-    this.interval = MS_IN_MINUTE / (this._bpm * PPQN)
+    this._pulseInterval = MS_IN_MINUTE / (this._bpm * PPQN)
   }
 
   onPulse(callback: () => void) {
@@ -27,9 +28,23 @@ export class MidiClockMock {
 
   private startLoop() {
     const pulse = () => {
-      if (this.isEnabled) this.pulseCallback?.()
-      setTimeout(pulse, this.interval)
+      if (this._lastPulseTimestamp === null) {
+        this._lastPulseTimestamp = performance.now()
+        return
+      }
+
+      const now = performance.now()
+      const delta = now - this._lastPulseTimestamp
+
+      if (delta > this._pulseInterval) {
+        const extraMs = delta - this._pulseInterval
+
+        // Subtract extraMs to correct the timestamp
+        this._lastPulseTimestamp = now - extraMs
+        if (this.isEnabled) this.pulseCallback?.()
+      }
     }
-    pulse()
+
+    setInterval(pulse, 0)
   }
 }
