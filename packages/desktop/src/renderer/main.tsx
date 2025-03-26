8b631a7fa42159adb91da37fff90b041eeee724d
Alex Kempton <alex@funwithtriangles.net>
2025-03-26 15:59:39 +0100

    css working

--- packages/desktop/src/renderer/main.tsx
+++ packages/desktop/src/renderer/main.tsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client'
 import '@hedron/ui-core/icons.css'
 import '@hedron/ui-core/fonts.css'
 import '@hedron/ui-core/base.css'
+import '@hedron/ui-core/modules.css'
 
 import { App } from '@components/App/App'
 
