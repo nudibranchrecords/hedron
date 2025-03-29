cb1f29f138d40228be47519c2059af6509398dbd
Cale Bradbury <cale.bradbury@gmail.com>
2025-03-29 19:47:34 -0400

    add support for ply imports

--- packages/desktop/src/main/SketchesServer.ts
+++ packages/desktop/src/main/SketchesServer.ts
@@ -85,6 +85,7 @@ export class SketchesServer extends EventEmitter {
         // text: loaded into sketch as string
         '.glsl': 'text',
         '.isf': 'text',
+        '.ply': 'text',
       },
       assetNames: '[dir]/[name]-[hash]',
       publicPath: `http://${HOST}:${port}`,
