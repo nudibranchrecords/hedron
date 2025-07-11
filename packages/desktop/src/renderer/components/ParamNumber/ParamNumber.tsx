fe830634c70d9e16544b28f02dac8e36977595b6
Alex Kempton <alex@funwithtriangles.net>
2025-07-11 19:04:27 +0200

    fix for typing number

--- packages/desktop/src/renderer/components/ParamNumber/ParamNumber.tsx
+++ packages/desktop/src/renderer/components/ParamNumber/ParamNumber.tsx
@@ -2,8 +2,9 @@ import { useRef } from 'react'
 import { useInterval } from 'usehooks-ts'
 import { FloatSlider } from '@hedron/ui-core'
 import type { FloatSliderHandle } from '@hedron/ui-core'
+import { NodeParamNumber } from '@hedron/engine'
 import { useOnNodeValueChange } from '@components/hooks/useOnNodeValueChange'
-import { engineStore } from '@renderer/engine'
+import { engineStore, useEngineStore } from '@renderer/engine'
 
 interface ParamNumberProps {
   id: string
@@ -11,15 +12,30 @@ interface ParamNumberProps {
 
 export const ParamNumber = ({ id }: ParamNumberProps) => {
   const ref = useRef<FloatSliderHandle>(null)
+  const prevVal = useRef<number | null>(null)
+
   const onValueChange = useOnNodeValueChange(id)
+
+  // const node = useEngineStore((state) => state.nodes[id] as NodeParamNumber)
 
   useInterval(() => {
     const nodeValue = engineStore.getState().nodeValues[id]
+    if (nodeValue === prevVal.current) {
+      return
+    }
     if (typeof nodeValue !== 'number') {
       throw new Error('ParamNumber value was not a number')
     }
     ref.current?.updateValue(nodeValue)
-  }, 100)
+    prevVal.current = nodeValue
+  }, 16)
 
-  return <FloatSlider ref={ref} onValueChange={onValueChange} />
+  return (
+    <FloatSlider
+      // min={node.sliderMin}
+      // max={node.sliderMax}
+      ref={ref}
+      onValueChange={onValueChange}
+    />
+  )
 }
