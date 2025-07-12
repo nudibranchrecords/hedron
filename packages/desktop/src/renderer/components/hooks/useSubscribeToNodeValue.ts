e584ed6ae3646ba0bca537f8f6ed4ce428719e2e
Alex Kempton <alex@funwithtriangles.net>
2025-07-12 10:08:46 +0200

    naive approach to vec3 updating

--- packages/desktop/src/renderer/components/hooks/useSubscribeToNodeValue.ts
+++ packages/desktop/src/renderer/components/hooks/useSubscribeToNodeValue.ts
@@ -1,6 +1,6 @@
 import { useEffect } from 'react'
-import { NodeValue } from '@hedron/engine'
-import { engineStore } from '@renderer/engine'
+import { NodeParamWithChildren, NodeValue } from '@hedron/engine'
+import { engineStore, useEngineStore } from '@renderer/engine'
 
 export const useSubscribeToNodeValue = <T extends NodeValue>(
   nodeId: string,
@@ -21,4 +21,34 @@ export const useSubscribeToNodeValue = <T extends NodeValue>(
       unsubscribe()
     }
   }, [nodeId, callback])
+}
+
+export const useSubscribeToNodeChildrenValues = <T extends NodeValue>(
+  nodeId: string,
+  callback: (value: T[]) => void,
+) => {
+  const { childNodeIds } = useEngineStore((state) => state.nodes[nodeId] as NodeParamWithChildren)
+
+  useEffect(() => {
+    const unsubscribeFuncs: (() => void)[] = []
+
+    childNodeIds.forEach((childNodeId) => {
+      const unsubscribe = engineStore.subscribe(
+        (state) => state.nodeValues[childNodeId],
+        () => {
+          const state = engineStore.getState()
+          const childValues = childNodeIds.map((id) => state.nodeValues[id]) as T[]
+          callback(childValues)
+        },
+        {
+          fireImmediately: true,
+        },
+      )
+      unsubscribeFuncs.push(unsubscribe)
+    })
+
+    return () => {
+      unsubscribeFuncs.forEach((unsubscribe) => unsubscribe())
+    }
+  }, [childNodeIds, callback, nodeId])
 }
