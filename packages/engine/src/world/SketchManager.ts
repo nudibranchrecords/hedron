c0569a36a6b492738230c7c9dec86f88fb3918c6
cale bradbury <cale.bradbury@gmail.com>
2025-07-17 04:00:28 -0400

    Sketch dispose function (#515)
    * Add dispose function, pass scene on create
    
    * pass engineScene to dispose
    
    ---------
    
    Co-authored-by: Alex Kempton <alex@funwithtriangles.net>

--- packages/engine/src/world/SketchManager.ts
+++ packages/engine/src/world/SketchManager.ts
@@ -16,13 +16,23 @@ type SketchInstance = {
   root?: Group
 
   getPasses?: (engineScene: EngineScene) => Pass[]
+
+  /**
+   * Called when the sketch is removed from the scene.
+   * It should clean up any resources, event listeners, or references it holds.
+   */
+  dispose(engineScene: EngineScene): () => void
 }
 
 export class SketchManager {
   private sketchInstances: { [id: string]: SketchInstance } = {}
 
-  private createSketch = (instanceId: string, module: SketchModule): SketchInstance => {
-    const sketch = new module(getDebugScene())
+  private createSketch = (
+    instanceId: string,
+    module: SketchModule,
+    scene: EngineScene,
+  ): SketchInstance => {
+    const sketch = new module(scene)
     if (sketch.root) {
       sketch.root.name = instanceId
     }
@@ -33,15 +43,17 @@ export class SketchManager {
   }
 
   public addSketchToScene = (instanceId: string, module: SketchModule): void => {
-    const scene = getDebugScene().scene
-    const sketch = this.createSketch(instanceId, module)
+    const engineScene = getDebugScene()
+    const scene = engineScene.scene
+    const sketch = this.createSketch(instanceId, module, engineScene)
     if (sketch.root) {
       scene.add(sketch.root)
     }
   }
 
   public removeSketchFromScene = (instanceId: string): void => {
-    const scene = getDebugScene().scene
+    const engineScene = getDebugScene()
+    const scene = engineScene.scene
     const oldSketch = scene.getObjectByName(instanceId)
 
     if (!oldSketch) {
@@ -49,6 +61,7 @@ export class SketchManager {
     }
 
     scene.remove(oldSketch)
+    this.sketchInstances[instanceId]?.dispose?.(engineScene)
     delete this.sketchInstances[instanceId]
   }
 
