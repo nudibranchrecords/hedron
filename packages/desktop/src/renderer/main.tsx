ced448b00dc2acadad680eb2c7ca5f0d920cf4db
Alex Kempton <alex@funwithtriangles.net>
2025-08-16 02:05:46 +0200

    Build / monorepo cleanup (#543)
    * move over to pnpm, building successfully
    
    * delete unecessary config stuff
    
    * tidy up package.json and readme
    
    * pr check yml update
    
    * yml update
    
    * dependency fixes
    
    * using peer dependencies for all react packages
    
    * more dep cleanup
    
    * tidy
    
    * dep simplify
    
    * lockfile update
    
    * simplified deps
    
    * fix for lint dep resolving
    
    * tsconfig cleanup
    
    * vite config unify
    
    * ts fixes
    
    * fix issue with stories
    
    * add build to checks
    
    * config tidy
    
    * vscode eslint setting fix
    
    * fix for vscode TS issue for test apps

--- packages/desktop/src/renderer/main.tsx
+++ packages/desktop/src/renderer/main.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom/client'
 
 import '@hedron/ui-core/icons.css'
 import '@hedron/ui-core/fonts.css'
