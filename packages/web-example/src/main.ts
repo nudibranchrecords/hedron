a317e67e6bb634eb443e1755b7a0a429c433dd49
Alex Kempton <alex@funwithtriangles.net>
2025-09-05 14:07:30 +0200

    handling LFO inputs

--- packages/web-example/src/main.ts
+++ packages/web-example/src/main.ts
@@ -1,4 +1,6 @@
 import { EngineData, HedronEngine } from '@hedron/engine'
+import { Clock } from '@hedron/clock'
+import { LFOInput } from '@hedron/lfo-input'
 import projectData from './project.json'
 import '@fontsource/chivo-mono'
 import './style.css'
@@ -6,10 +8,17 @@ import { getSketchModuleItems } from './utils'
 
 const engineData = projectData.engine as unknown as EngineData
 
+const clock = new Clock()
+
+clock.start()
+
 const engine = new HedronEngine({
   rendererType: 'webgl',
   canvasSizeMode: 'fillContainer',
+  clock,
 })
+
+engine.registerPlugin(new LFOInput(engine))
 
 const engineStore = engine.getStore()
 const { setSketchModuleItem, loadProject } = engineStore.getState()
