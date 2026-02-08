794d10a78b542ec0a8e2bd13762eaad15ea9e05e
Alex Kempton <alex@funwithtriangles.net>
2026-02-08 03:03:17 +0100

    lint fix

--- apps/desktop/src/renderer/components/SketchControls/SketchControls.tsx
+++ apps/desktop/src/renderer/components/SketchControls/SketchControls.tsx
@@ -1,5 +1,5 @@
 import { ControlGrid, Collapsible, HedronErrorBoundary } from '@hedron-gl/ui-core'
-import { ComponentType, useMemo } from 'react'
+import { ComponentType } from 'react'
 import { Node } from '@hedron-gl/engine'
 import c from './SketchControls.module.css'
 import { useAppStore } from '@renderer/appStore'
@@ -17,7 +17,8 @@ interface SketchControlsProps {
 const EMPTY_OBJECT = {} as Record<number, boolean>
 
 export const SketchControls = ({ sketchId, nodeGroups, ControlItem }: SketchControlsProps) => {
-  const openedControlGroups = useAppStore((state) => state.openedControlGroups[sketchId]) ?? EMPTY_OBJECT
+  const openedControlGroups =
+    useAppStore((state) => state.openedControlGroups[sketchId]) ?? EMPTY_OBJECT
   const setOpenedControlGroup = useAppStore((state) => state.setOpenedControlGroup)
 
   return (
