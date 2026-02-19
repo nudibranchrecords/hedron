c099fa040e41b6e03ffbac446e124cd42d7deb3e
Alex Kempton <alex@funwithtriangles.net>
2026-02-19 09:28:06 +0100

    simplified vector2 and 3 components

--- packages/ui-core/src/containers/NodeContainer/ParamVector2/ParamVector2.tsx
+++ packages/ui-core/src/containers/NodeContainer/ParamVector2/ParamVector2.tsx
@@ -1,24 +1,10 @@
-import { useRef } from 'react'
 import { NodeParamVector2 } from '@hedron-gl/engine'
 import c from './ParamVector2.module.css'
-import { FloatSlider, FloatSliderHandle } from '@components/FloatSlider/FloatSlider'
-import { useOnNodeValueChange } from '@hooks/useOnNodeValueChange'
-import { useSubscribeToNodeValue } from '@hooks/useSubscribeToNodeValue'
+import { ParamNumber } from '@containers/NodeContainer/ParamNumber/ParamNumber'
 import { useEngineStore } from '@hooks/storeHooks'
 
 interface ParamVector2Props {
   id: string
-}
-
-const SingleSlider = ({ id }: ParamVector2Props) => {
-  const ref = useRef<FloatSliderHandle>(null)
-  const onValueChange = useOnNodeValueChange(id)
-
-  useSubscribeToNodeValue<number>(id, (value) => {
-    ref.current?.updateValue(value)
-  })
-
-  return <FloatSlider ref={ref} onValueChange={onValueChange} />
 }
 
 export const ParamVector2 = ({ id }: ParamVector2Props) => {
@@ -30,7 +16,7 @@ export const ParamVector2 = ({ id }: ParamVector2Props) => {
     <div className={c.container}>
       {childNodeIds.map((childId) => (
         <div key={childId} className={c.item}>
-          <SingleSlider key={childId} id={childId} />
+          <ParamNumber key={childId} id={childId} />
         </div>
       ))}
     </div>
