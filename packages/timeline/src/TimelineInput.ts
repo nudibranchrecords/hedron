cc6beede203a0b850178515e5c85f4f348de8fc7
Alex Kempton <alex@funwithtriangles.net>
2026-04-24 12:51:26 +0200

    engine.addNode replaces engine.addCustomNode

--- packages/timeline/src/TimelineInput.ts
+++ packages/timeline/src/TimelineInput.ts
@@ -12,7 +12,8 @@ export class TimelineInput implements IPlugin {
   onEngineInitialize(engine: HedronEngine) {
     const timelineNodeId = `${this.id}-default-timeline`
     // TODO: Ensure this only gets added once, either with logic here or in the engine's addCustomNode method
-    engine.addCustomNode<TimelineNode>(timelineNodeId, null, {
+    engine.addNode<TimelineNode>(timelineNodeId, null, {
+      isCustomNode: true,
       nodeType: 'timeline',
       customData: {
         durationMs: 60000,
