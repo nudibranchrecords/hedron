5e31e098c3d182fab20bd404b5295a688f669cdd
Alex Kempton <alex@funwithtriangles.net>
2026-05-21 13:25:04 +0100

    ParamFile in NodeContainer

---
+++ packages/ui-core/src/containers/NodeContainer/ParamFile/ParamFile.tsx
@@ -0,0 +1,25 @@
+import { ParamFile as ParamFileType, ParamFileValue } from '@hedron-gl/engine'
+import { useAppStoreShallow, useEngineStore } from '@hooks/engineHooks'
+import { useOnParamValueChange } from '@hooks/useOnParamValueChange'
+import { useParamValue } from '@hooks/useParamValue'
+import { FilePicker } from '@components/FilePicker/FilePicker'
+
+interface ParamFileProps {
+  id: string
+}
+
+export const ParamFile = ({ id }: ParamFileProps) => {
+  const node = useEngineStore((state) => state.nodes[id]) as ParamFileType
+  const currentFile = useParamValue<ParamFileValue>(id)
+  const onFileChange = useOnParamValueChange(id)
+  const availableFiles = useAppStoreShallow((state) => Object.values(state.resourcesFiles))
+
+  return (
+    <FilePicker
+      currentFile={currentFile}
+      onFileChange={onFileChange}
+      availableFiles={availableFiles}
+      accept={node.accept}
+    />
+  )
+}
