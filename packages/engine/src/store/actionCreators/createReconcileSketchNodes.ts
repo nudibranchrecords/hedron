967833e058013672f41a826a6fd4ecfbd2ea5d82
Alex Kempton <alex@funwithtriangles.net>
2026-06-18 11:19:55 +0200

    tidy

--- packages/engine/src/store/actionCreators/createReconcileSketchNodes.ts
+++ packages/engine/src/store/actionCreators/createReconcileSketchNodes.ts
@@ -25,12 +25,6 @@ export const createReconcileSketchNodes: SetterCreator<'reconcileSketchNodes'> =
       // 1. Add new nodes that are in the config but not in the current sketch.
       for (const nodeConfig of config.nodes) {
         // Find existing node for this key, if any.
-
-        // FIXME: There should never be a custom node as part of a sketch, types should know this
-        if (nodeConfig.nodeType === 'custom') {
-          continue
-        }
-
         let nodeId = findNodeWithKeyFromIdList(
           state.nodes,
           nodeConfig.key,
