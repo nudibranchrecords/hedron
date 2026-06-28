cfb868e3b3789469a53433aa385117515695f9e8
Alex Kempton <alex@funwithtriangles.net>
2026-06-28 10:51:47 +0200

    removed active region for keyframes

--- packages/timeline/src/components/Timeline/TrackKeyframes.tsx
+++ packages/timeline/src/components/Timeline/TrackKeyframes.tsx
@@ -1,5 +1,4 @@
 import { Keyframe } from './Keyframe'
-import c from './Timeline.module.css'
 import { TimelineManagerKeyframeTrack } from '@/types'
 
 interface TrackKeyframesProps {
@@ -16,19 +15,6 @@ export const TrackKeyframes = ({
   setSelectedKeyframes,
 }: TrackKeyframesProps) => (
   <>
-    {track.keyframes.map((kf, i) => {
-      if (!kf.value) return null
-      const startPercent = (kf.time / durationMs) * 100
-      const nextKf = track.keyframes[i + 1]
-      const endPercent = nextKf ? (nextKf.time / durationMs) * 100 : 100
-      return (
-        <div
-          key={`region-${kf.id}`}
-          className={c.activeRegion}
-          style={{ left: `${startPercent}%`, width: `${endPercent - startPercent}%` }}
-        />
-      )
-    })}
     {track.keyframes.map((kf) => {
       const percent = (kf.time / durationMs) * 100
       const isKeyframeSelected = selectedKeyframes?.includes(kf.id) ?? false
